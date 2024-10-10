3f2b3317f07f222fa57e42ab9cd33a0259ee46cc
bernardotonin <btprates2004@gmail.com>
2024-10-10 14:44:24 -0300

    chore: configure tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   presets: [require('nativewind/preset')],
   theme: {
     extend: {},
+    colors: {
+      'tl-bg': '#171717',
+      'tab-bg': '#151515',
+      'tl-purple': '#9400da',
+      'tl-red': '#f50437',
+      'tl-red-2': '#bf0028',
+      'tl-purple-2': '#7d00b8',
+    },
   },
   plugins: [],
 };
