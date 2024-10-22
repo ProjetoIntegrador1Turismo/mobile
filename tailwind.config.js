da9f4b957bf8bd911d2e3f4fbea3eb59cdd4ebb3
bernardotonin <btprates2004@gmail.com>
2024-10-22 15:08:49 -0300

    chore: correct tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,15 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
-    colors: {
-      'tl-bg': '#171717',
-      'tab-bg': '#151515',
-      'tl-purple': '#9400da',
-      'tl-red': '#f50437',
-      'tl-red-2': '#bf0028',
-      'tl-purple-2': '#7d00b8',
+    extend: {
+      colors: {
+        'tl-bg': '#0F0F0F',
+        'tab-bg': '#151515',
+        'tl-purple': '#9400da',
+        'tl-red': '#f50437',
+        'tl-red-2': '#bf0028',
+        'tl-purple-2': '#7d00b8',
+      },
     },
   },
   plugins: [],
