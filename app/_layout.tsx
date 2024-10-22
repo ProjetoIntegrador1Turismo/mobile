9394e677eaaa444f03079d3bb9a4baacd31d0c61
bernardotonin <btprates2004@gmail.com>
2024-10-22 15:10:24 -0300

    chore: comment splash screen logic for development purposes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { Stack } from 'expo-router';
 
 import '../global.css';
-import { useSplashStore } from '~/src/common/stores/SplashStore';
-import { SplashView } from '~/src/screens/Splash/SplashView';
+// import { useSplashStore } from '~/src/common/stores/SplashStore';
+// import { SplashView } from '~/src/screens/Splash/SplashView';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -10,13 +11,22 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const splashComplete = useSplashStore((state) => state.splashComplete);
+  // splash screen code
+  // const splashComplete = useSplashStore((state) => state.splashComplete);
 
-  return splashComplete ? (
-    <Stack screenOptions={{ contentStyle: { backgroundColor: '#151515' }}}>
+  // return splashComplete ? (
+  //   <Stack>
+  //     <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+  //     <Stack.Screen name='(auth)/register' options={{ headerShown: false }} />
+  //   </Stack>
+  // ) : (
+  //   <SplashView />
+  // );
+
+  return (
+    <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen name='(auth)/register' options={{ headerShown: false }} />
     </Stack>
-  ) : (
-    <SplashView />
   );
 }
