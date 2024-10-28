088c85212d2ef17b17946a1e4205d0c834e2d96b
bernardotonin <btprates2004@gmail.com>
2024-10-28 15:05:13 -0300

    feat: create test login view

---
+++ src/screens/Login/LoginView.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'expo-router';
+import React from 'react';
+import { View, Text, SafeAreaView, Pressable } from 'react-native';
+
+export default function LoginView() {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+      <Text className='text-3xl text-white'>login</Text>
+      <Link
+        href={{
+          pathname: '/(auth)/register',
+        }}
+        asChild>
+        <Pressable className='border-black border p-3'>
+          <Text>registro</Text>
+        </Pressable>
+      </Link>
+    </SafeAreaView>
+  );
+}
