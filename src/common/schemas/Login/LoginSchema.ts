98525f6c72e7064b8bb5b875840c8b9090100cd6
bernardotonin <btprates2004@gmail.com>
2024-10-31 16:52:09 -0300

    feat: create login schema

---
+++ src/common/schemas/Login/LoginSchema.ts
@@ -0,0 +1,8 @@
+// const LoginSchema = z.object({
+//   email: z
+//     .string({ required_error: 'E-mail é obrigatorio!' })
+//     .email({ message: 'Insira um e-mail válido!' }),
+//   password: z.string().min(5, { message: 'Senha deve ter no minimo 5 caracteres!' })
+// });
+
+// export default LoginSchema;
