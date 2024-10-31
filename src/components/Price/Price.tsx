eb9f223f09505c32a34acd37f87e9e72475de3da
Guilherme <guilherme_laz@hotmail.com>
2024-10-31 15:02:02 -0300

    feat: extracted price from TopInterestPointCard and created a new component

---
+++ src/components/Price/Price.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { PriceProps } from 'src/components/Price/Price.types';
+
+export function Price({ priceLevel }: PriceProps) {
+  return (
+    <View className='flex flex-row space-x-1'>
+      {Array.from({ length: 3 }, (_, index) => (
+        <Feather
+          key={index}
+          name='dollar-sign'
+          size={12}
+          color={index < priceLevel ? 'black' : 'gray'}
+        />
+      ))}
+    </View>
+  );
+}
