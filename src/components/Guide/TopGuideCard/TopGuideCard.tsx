729bb83ebfdbbc19e3eb7fcfdccd7bbcaca0b2c9
Guilherme <guilherme_laz@hotmail.com>
2024-10-31 16:15:14 -0300

    Feat: added TopGuideCard

---
+++ src/components/Guide/TopGuideCard/TopGuideCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { AntDesign, Entypo } from '@expo/vector-icons';
+import { useTopGuideCardViewModel } from './TopGuideCardViewModel';
+import { TopGuideCardProps } from 'src/components/Guide/TopGuideCard/TopGuideCard.types';
+import { CustomText } from 'src/components/Text/CustomText';
+
+export function TopGuideCard({ id, profileImage, name, rating }: TopGuideCardProps) {
+  const { stars } = useTopGuideCardViewModel(rating);
+
+  return (
+    <View className='flex h-[55px] w-[362px] flex-row items-center rounded-xl border border-red-700 bg-[#1C1C1E] px-2 py-2'>
+      <Image
+        source={{ uri: profileImage }}
+        style={{ width: 41, height: 41 }}
+        className='rounded-full'
+      />
+
+      <View className='ml-4  flex-1 overflow-hidden'>
+        <CustomText size={14} color='white' weight='regular'>
+          {name}
+        </CustomText>
+      </View>
+
+      <View className='mr-4 flex flex-row items-center space-x-1'>
+        <Text className='text-white'>Avaliação:</Text>
+        <View className='flex flex-row'>
+          {stars.map((filled, index) => (
+            <AntDesign key={index} name='star' size={14} color={filled ? '#FF007F' : 'gray'} />
+          ))}
+        </View>
+      </View>
+
+      <Entypo name='dots-three-horizontal' size={20} color='white' className='ml-4' />
+    </View>
+  );
+}
