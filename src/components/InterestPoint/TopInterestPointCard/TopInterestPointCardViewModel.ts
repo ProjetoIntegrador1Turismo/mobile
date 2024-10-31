332fb5a9146f8000028fd8feb514a119d450eea3
Guilherme <guilherme_laz@hotmail.com>
2024-10-31 14:32:19 -0300

    feat: Added TopInterestPointCardViewModel

---
+++ src/components/InterestPoint/TopInterestPointCard/TopInterestPointCardViewModel.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+
+export const getMedalColor: Record<number, string> = {
+  1: 'gold',
+  2: 'silver',
+  3: 'white',
+};
+
