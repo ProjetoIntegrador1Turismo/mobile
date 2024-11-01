8d3bef45c85950ddffdd21fc00f1923f8857c0cb
Bernardo Tonin <btprates2004@gmail.com>
2024-10-31 21:20:05 -0300

    feat: finish login view styling

--- src/screens/Login/LoginView.tsx
+++ src/screens/Login/LoginView.tsx
@@ -7,27 +7,36 @@ import { Container } from 'src/components/Container/Container';
 import { CustomText } from 'src/components/Text/CustomText';
 
 import { LoginForm } from '~/src/components/Auth/LoginForm/LoginForm';
+import TLLogoWhite from '~/src/components/Auth/TLLogoWhite/TLLogoWhite';
 import { SolidButton } from '~/src/components/Button/SolidButton/SolidButton';
 import { TLGradientButton } from '~/src/components/Button/TLGradientButton/TLGradientButton';
 
 export default function LoginView() {
   return (
     <FormContainer className='items-center bg-tl-bg'>
-      <View className='mt-16 flex w-11/12'>
+      <View className='mt-8 flex w-11/12 gap-6'>
         <GoBackButton />
-        <CustomText className='text-white' size={36} weight='bold'>
-          Acesse
-        </CustomText>
-        <CustomText size={20} className='text-white'>
-          Insira e-mail e senha para entrar!
-        </CustomText>
+        <View>
+          <CustomText className='text-white' size={36} weight='bold'>
+            Acesse
+          </CustomText>
+          <CustomText size={20} className='text-white'>
+            Insira e-mail e senha para entrar!
+          </CustomText>
+        </View>
       </View>
-      <LoginForm />
-      <CustomText className='text-white'>Esqueci minha senha</CustomText>
-      <View className='flex w-[75%] flex-row justify-between'>
-        <TLGradientButton title='Acessar' />
-        <SolidButton title='Criar minha conta' />
+      <View className='mb-3 mt-14'>
+        <LoginForm />
       </View>
+      <View className='mb-10 flex w-[90%] flex-row justify-between'>
+        <CustomText className='text-white'>Lembrar minha senha</CustomText>
+        <CustomText className='text-white'>Esqueci minha senha</CustomText>
+      </View>
+      <View className='flex w-[90%] flex-row justify-between'>
+        <TLGradientButton title='Acessar' className='min-w-[40%]' />
+        <SolidButton title='Criar conta' className='min-w-[40%]' />
+      </View>
+      <TLLogoWhite className='mt-[140px] h-[100px] w-[150px] object-cover' />
     </FormContainer>
   );
 }
