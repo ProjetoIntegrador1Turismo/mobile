ea1da69f9c8e09aae76d02e10af33d80c28fb4c2
Bernardo Tonin <btprates2004@gmail.com>
2024-10-31 21:21:42 -0300

    style: adjust login form gap

--- src/components/Auth/LoginForm/LoginForm.tsx
+++ src/components/Auth/LoginForm/LoginForm.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native';
+
 import { ControlledInput } from '../ControlledInput/ControlledInput';
 
 export function LoginForm() {
   return (
-    <View>
+    <View className='flex gap-1'>
       <ControlledInput label='E-mail' placeholder='Digite seu e-mail' />
       <ControlledInput label='Password' placeholder='Digite sua senha' />
     </View>
