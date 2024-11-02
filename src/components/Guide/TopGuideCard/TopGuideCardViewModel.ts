8804f37a9d01eda039abf3e602efc4585931cd39
Guilherme <guilherme_laz@hotmail.com>
2024-11-02 20:03:41 -0300

    Feat: added TopGuideCardViewModel

--- src/components/Guide/TopGuideCard/TopGuideCardViewModel.ts
+++ src/components/Guide/TopGuideCard/TopGuideCardViewModel.ts
@@ -1,4 +1,8 @@
+import { useAppRouter } from 'src/common/lib/router';
+
 export function useTopGuideCardViewModel(rating: number) {
+  const { push } = useAppRouter();
+
   const roundedRating = Math.round(rating);
 
   const stars = Array.from({ length: 5 }, (_, index) => index < roundedRating);
