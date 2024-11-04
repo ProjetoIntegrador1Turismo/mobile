33dcc8137cee2cc7bbff52c7bec7a05ddaf6fbd8
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-04 15:54:00 -0300

    style: updates CustomText position in CategoryCard according to design

--- src/components/CategoryCardList/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCardList/CategoryCard/CategoryCard.tsx
@@ -7,7 +7,7 @@ export function CategoryCard({ title, imgSource, className }: CategoryCardProps)
   return (
     <View className={`h-[90px] w-[362px] overflow-hidden rounded-2xl ${className}`}>
       <Image source={imgSource} className='h-full w-full object-cover opacity-90' />
-      <View className='absolute left-[25px] top-[8px]'>
+      <View className='absolute left-[30px] top-[19px]'>
         <CustomText weight='bold' size={24} color='#FFFFFF'>
           {title}
         </CustomText>
