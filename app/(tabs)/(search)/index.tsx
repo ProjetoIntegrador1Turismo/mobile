43fa7e135cf150672f8ac0ea25180f5921bfbf0e
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-04 15:54:56 -0300

    chore: soft insert CategoryCardList on Search tab

--- app/(tabs)/(search)/index.tsx
+++ app/(tabs)/(search)/index.tsx
@@ -1,6 +1,7 @@
 import { Pressable, SafeAreaView } from 'react-native';
 import { useAppRouter } from 'src/common/lib/router';
 import { CustomText } from 'src/components/Text/CustomText';
+import { CategoryCardList } from '~/src/components/CategoryCardList/CategoryCardList';
 
 export default function Search() {
   const { push } = useAppRouter();
@@ -11,10 +12,11 @@ export default function Search() {
 
   return (
     <SafeAreaView className='flex-1 items-center justify-center p-8'>
-      <CustomText>Categories/Search Screen</CustomText>
+      {/* <CustomText>Categories/Search Screen</CustomText>
       <Pressable onPress={handlePress} className='rounded-lg border border-black p-3'>
         <CustomText>Go to Point</CustomText>
-      </Pressable>
+      </Pressable> */}
+      <CategoryCardList />
     </SafeAreaView>
   );
 }
