a83c61f911b09679714f82e9ab5143c476365bd2
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-04 15:43:11 -0300

    style: define on CategoryCardList a 20px bottom margin and remove ItemSeparatorComponent

--- src/components/CategoryCardList/CategoryCardList.tsx
+++ src/components/CategoryCardList/CategoryCardList.tsx
@@ -1,23 +1,18 @@
 import { FlatList, View } from 'react-native';
-import { CategoryCard } from '~/src/components/CategoryCard/CategoryCard';
-import { CategoryCardData } from '~/src/components/CategoryCard/CategoryCardViewModel'; 
-import { CategoryCardProps } from '~/src/components/CategoryCard/CategoryCard.types';
+import { CategoryCard } from '~/src/components/CategoryCardList/CategoryCard/CategoryCard';
+import { CategoryCardData } from '~/src/components/CategoryCardList/CategoryCard/CategoryCardViewModel';
+import { CategoryCardProps } from '~/src/components/CategoryCardList/CategoryCard/CategoryCard.types';
 import { CustomText } from '~/src/components/Text/CustomText';
 
-
 export function CategoryCardList() {
-    return (
-      <View className='w-[362px]'>
-        <CustomText size={36} >Categorias:</CustomText>
-        <FlatList
-          data={CategoryCardData}
-          keyExtractor={(card: CategoryCardProps) => card.title}
-          renderItem={({ item }) => (
-            <CategoryCard title={item.title} imgSource={item.imgSource} />
-          )}
-          showsVerticalScrollIndicator={false}
-          ItemSeparatorComponent={() => <View className='h-[20px]' />}          
-        />
-      </View>
-    );
-  }
+  return (
+    <View className='w-[362px]'>
+      <CustomText size={36}>Categorias:</CustomText>
+      <FlatList
+        data={CategoryCardData}
+        keyExtractor={(card: CategoryCardProps) => card.title}
+        renderItem={({ item }) => <CategoryCard title={item.title} imgSource={item.imgSource} className='mb-[20px]'/>}
+      />
+    </View>
+  );
+}
