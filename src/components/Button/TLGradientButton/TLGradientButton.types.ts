0baeaaf6fb4a4b7738f957cf3adbded8b5c69089
bernardotonin <btprates2004@gmail.com>
2024-11-05 18:57:10 -0300

    feat : add onpress and disabled to tlgradient button props

--- src/components/Button/TLGradientButton/TLGradientButton.types.ts
+++ src/components/Button/TLGradientButton/TLGradientButton.types.ts
@@ -1,4 +1,8 @@
+import { GestureResponderEvent } from 'react-native';
+
 export interface TLGradientButtonProps {
   title: string;
   className?: string;
+  disabled?: boolean;
+  onPress?: (event: GestureResponderEvent) => void;
 }
