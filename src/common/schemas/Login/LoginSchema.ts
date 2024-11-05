6fd93e487cd8642f55a6dc306da00382f839be48
bernardotonin <btprates2004@gmail.com>
2024-11-05 18:53:51 -0300

    feat: create login schema

--- src/common/schemas/Login/LoginSchema.ts
+++ src/common/schemas/Login/LoginSchema.ts
@@ -1,8 +1,10 @@
-// const LoginSchema = z.object({
-//   email: z
-//     .string({ required_error: 'E-mail é obrigatorio!' })
-//     .email({ message: 'Insira um e-mail válido!' }),
-//   password: z.string().min(5, { message: 'Senha deve ter no minimo 5 caracteres!' })
-// });
+import { z } from 'zod';
 
-// export default LoginSchema;
+const LoginSchema = z.object({
+  username: z
+    .string({ required_error: 'E-mail é obrigatorio!' })
+    .email({ message: 'Insira um e-mail válido!' }),
+  password: z.string().min(5, { message: 'Senha deve ter no minimo 5 caracteres!' }),
+});
+
+export default LoginSchema;
