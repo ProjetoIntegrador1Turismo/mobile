def04bc7d6b7e85e818bb13e91d4c348d240f7d0
bernardotonin <btprates2004@gmail.com>
2024-11-07 19:02:15 -0300

    feat: add async storage test

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,17 +1,23 @@
-import { Pressable, SafeAreaView } from 'react-native';
+import { Pressable, SafeAreaView, Alert } from 'react-native';
 import { useAppRouter } from 'src/common/lib/router';
 import { useAuthStore } from 'src/common/stores/AuthStore';
 import { CustomText } from 'src/components/Text/CustomText';
 
+import { getObject } from '~/src/common/lib/storage';
 import { Login } from '~/src/common/repositories/auth/auth.repository';
 
 export default function Home() {
   const { push } = useAppRouter();
   const user = useAuthStore((state) => state.user);
-  const logout = useAuthStore((state) => state.logout);
+  const { logout, error } = useAuthStore();
 
   const handlePress = async () => {
     await logout();
+  };
+
+  const handleStoragePress = async () => {
+    const data = await getObject('user');
+    Alert.alert(JSON.stringify(data));
   };
 
   return (
@@ -20,9 +26,13 @@ export default function Home() {
       <Pressable onPress={handlePress} className='rounded-lg border border-black p-3'>
         <CustomText>Logout</CustomText>
       </Pressable>
+      <Pressable onPress={handleStoragePress} className='rounded-lg border border-black p-3'>
+        <CustomText>Get from storage</CustomText>
+      </Pressable>
       <CustomText className='max-h-[250px] overflow-hidden'>
         {JSON.stringify(user, null, 2)}
       </CustomText>
+      <CustomText>{JSON.stringify(error, null, 2)}</CustomText>
     </SafeAreaView>
   );
 }
