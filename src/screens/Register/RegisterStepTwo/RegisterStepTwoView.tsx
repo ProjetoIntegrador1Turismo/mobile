a3b9a5800d150f3bdc81e57a75a20de358c643f8
bernardotonin <btprates2004@gmail.com>
2024-11-07 19:07:49 -0300

    feat: create register step two view

---
+++ src/screens/Register/RegisterStepTwo/RegisterStepTwoView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'src/components/Container/Container';
+import { CustomText } from 'src/components/Text/CustomText';
+
+export function RegisterStepTwoView() {
+  return (
+    <Container className='items-center justify-center'>
+      <CustomText>Register Step Two</CustomText>
+    </Container>
+  );
+}
