6c5a213ce0102f1d843751cabb97edb65f597953
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-08 17:23:42 -0300

    refactor: insert TouchableOpacity moved from CatogoryCardList and use cn util to deal with tailwind class merge

--- src/components/CategoryCardList/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCardList/CategoryCard/CategoryCard.tsx
@@ -1,11 +1,16 @@
-import { Image, View } from 'react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { CustomText } from '~/src/components/Text/CustomText';
 
 import { CategoryCardProps } from '~/src/components/CategoryCardList/CategoryCard/CategoryCard.types';
+import { onPressCategoryCard } from '~/src/components/CategoryCardList/CategoryCard/CategoryCardViewModel'
+import { cn } from '~/src/common/utils/cn';
 
 export function CategoryCard({ title, imgSource, className }: CategoryCardProps) {
   return (
-    <View className={`h-[90px] w-[362px] overflow-hidden rounded-2xl ${className}`}>
+    <TouchableOpacity
+    onPress={() => onPressCategoryCard(title)}
+    >  
+    <View className={cn('h-[90px] w-[362px] overflow-hidden rounded-2xl', className)}>      
       <Image source={imgSource} className='h-full w-full object-cover opacity-90' />
       <View className='absolute left-[30px] top-[19px]'>
         <CustomText weight='bold' size={24} color='#FFFFFF'>
@@ -13,5 +18,6 @@ export function CategoryCard({ title, imgSource, className }: CategoryCardProps)
         </CustomText>
       </View>
     </View>
+    </TouchableOpacity>
   );
 }
