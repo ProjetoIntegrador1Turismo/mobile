3e102227f13f60527a061d4d2b74b67b74d711f2
Guilherme <guilherme_laz@hotmail.com>
2024-11-09 20:55:46 -0300

    chore: remove unnecessary import

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { DividerProps } from 'src/components/Divider/Divider.types';
 import { CustomText } from '../Text/CustomText';
 
