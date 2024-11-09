068b7a47f8ae140f51959ea222fd91cf6b99fb34
Guilherme <guilherme_laz@hotmail.com>
2024-11-09 20:56:14 -0300

    chore: extracted Stars component and its logic

--- src/components/Guide/TopGuideCard/TopGuideCard.tsx
+++ src/components/Guide/TopGuideCard/TopGuideCard.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import { View, Image, Text, Pressable } from 'react-native';
-import { AntDesign, Entypo } from '@expo/vector-icons';
-import { useTopGuideCardViewModel } from './TopGuideCardViewModel';
+import { View, Pressable } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import { TopGuideCardProps } from 'src/components/Guide/TopGuideCard/TopGuideCard.types';
 import { CustomText } from 'src/components/Text/CustomText';
 import { Avatar } from 'src/components/Avatar/Avatar';
-export function TopGuideCard({ id, profileImage, name, rating }: TopGuideCardProps) {
-  const { stars } = useTopGuideCardViewModel(rating);
+import { Stars } from 'src/components/Stars/Stars';
 
+export function TopGuideCard({ id, profileImage, name, rating }: TopGuideCardProps) {
   return (
     <View className='flex h-[55px] w-[362px] flex-row items-center rounded-xl border border-white bg-[#1C1C1E] px-2 py-2'>
       <Avatar imageUrl={profileImage} size={40} />
 
-      <View className='ml-4  flex-1 overflow-hidden'>
+      <View className='ml-4  flex-auto overflow-hidden'>
         <CustomText size={14} weight='regular' className='text-white '>
           {name}
         </CustomText>
       </View>
 
-      <View className='flex flex-row items-center space-x-1'>
-        <Text className='text-white'>Avaliação:</Text>
-        <View className='flex flex-row'>
-          {stars.map((filled, index) => (
-            <AntDesign key={index} name='star' size={14} color={filled ? '#FF007F' : 'gray'} />
-          ))}
-        </View>
-      </View>
+      <Stars rating={rating} />
 
       <Pressable onPress={() => {}}>
         <Entypo name='dots-three-horizontal' size={20} color='white' className='ml-4' />
