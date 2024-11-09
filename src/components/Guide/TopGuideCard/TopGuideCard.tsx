66413f0d05012143761faf9197f27e9e6bdb11a7
Guilherme <guilherme_laz@hotmail.com>
2024-11-09 19:50:43 -0300

    chore: update to use avatar component

--- src/components/Guide/TopGuideCard/TopGuideCard.tsx
+++ src/components/Guide/TopGuideCard/TopGuideCard.tsx
@@ -4,17 +4,13 @@ import { AntDesign, Entypo } from '@expo/vector-icons';
 import { useTopGuideCardViewModel } from './TopGuideCardViewModel';
 import { TopGuideCardProps } from 'src/components/Guide/TopGuideCard/TopGuideCard.types';
 import { CustomText } from 'src/components/Text/CustomText';
-
+import { Avatar } from 'src/components/Avatar/Avatar';
 export function TopGuideCard({ id, profileImage, name, rating }: TopGuideCardProps) {
   const { stars } = useTopGuideCardViewModel(rating);
 
   return (
     <View className='flex h-[55px] w-[362px] flex-row items-center rounded-xl border border-white bg-[#1C1C1E] px-2 py-2'>
-      <Image
-        source={{ uri: profileImage }}
-        style={{ width: 41, height: 41 }}
-        className='rounded-full'
-      />
+      <Avatar imageUrl={profileImage} size={40} />
 
       <View className='ml-4  flex-1 overflow-hidden'>
         <CustomText size={14} weight='regular' className='text-white '>
