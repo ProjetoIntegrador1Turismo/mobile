b41b95287ccf446996b6a81e0418400bc3227221
Guilherme <guilherme_laz@hotmail.com>
2024-11-09 20:57:25 -0300

    chore: added cn on classname for micro adjustments

--- src/components/Price/Price.tsx
+++ src/components/Price/Price.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { PriceProps } from 'src/components/Price/Price.types';
+import { cn } from 'src/common/utils/cn';
 
-export function Price({ priceLevel }: PriceProps) {
+export function Price({ priceLevel, className }: PriceProps) {
   return (
-    <View className='flex flex-row space-x-1'>
+    <View className={cn('flex flex-row space-x-1', className)}>
       {Array.from({ length: 3 }, (_, index) => (
         <Feather
           key={index}
