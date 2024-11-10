169a697e962329c9517c62959307d828c765b1bf
Guilherme <guilherme_laz@hotmail.com>
2024-11-09 22:38:43 -0300

    feat: added GuideItineraryCard Props

---
+++ src/components/Guide/GuideItineraryCard/GuideItineraryCard.types.ts
@@ -0,0 +1,12 @@
+// GuideItineraryCard.types.ts
+
+export interface GuideItineraryCardProps {
+    guide: {
+      name: string;
+      profileImage: string;
+    };
+    title: string;
+    backgroundImage: string;
+    onPress: () => void;
+  }
+  
