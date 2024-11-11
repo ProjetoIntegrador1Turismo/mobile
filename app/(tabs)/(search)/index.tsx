fb0c3ec1098c16bc7f08dbe0d5be5110527e59b7
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-11 16:26:10 -0300

    chore: insert NotifiedInterestCard on search tab to work

--- app/(tabs)/(search)/index.tsx
+++ app/(tabs)/(search)/index.tsx
@@ -1,8 +1,11 @@
 import { Pressable, SafeAreaView } from 'react-native';
 import { useAppRouter } from 'src/common/lib/router';
 import { CustomText } from 'src/components/Text/CustomText';
+import { Avatar } from '~/src/components/Avatar/Avatar';
 import { CategoryCardList } from '~/src/components/CategoryCardList/CategoryCardList';
+import { NotifiedInterestCard } from '~/src/components/NotifiedInterestCard/NotifiedInterestCard';
 import { SearchTextInputBar } from '~/src/components/SearchTextInputBar/SearchTextInputBar';
+import { UserAvatar } from '~/src/components/User/UserAvatar';
 
 export default function Search() {
   const { push } = useAppRouter();
@@ -16,9 +19,20 @@ export default function Search() {
       {/* <CustomText>Categories/Search Screen</CustomText>
       <Pressable onPress={handlePress} className='rounded-lg border border-black p-3'>
         <CustomText>Go to Point</CustomText>
-      </Pressable> */}
-      <SearchTextInputBar />
-      <CategoryCardList />
+      </Pressable> */}            
+      <NotifiedInterestCard
+      userName='Carlos Alberto de Nobrega'
+      imageUrl='https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      phone='(45) 99999-1111'
+      email='pracaenossa@gmail.com'
+      itineraryTitle='Aventura nas Cataratas' />
     </SafeAreaView>
   );
 }
+
+
+// userName: string,
+//     imageUrl: string,
+//     phone?: string,
+//     email?: string,
+//     itineraryTitle?: string
