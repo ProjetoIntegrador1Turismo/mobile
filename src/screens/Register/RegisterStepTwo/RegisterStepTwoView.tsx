832a3ff0582da3ee710491b7bf953610dd4c5c46
bernardotonin <btprates2004@gmail.com>
2024-11-12 18:55:32 -0300

    feat: testing dynamic form

--- src/screens/Register/RegisterStepTwo/RegisterStepTwoView.tsx
+++ src/screens/Register/RegisterStepTwo/RegisterStepTwoView.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Container } from 'src/components/Container/Container';
+import { useWatch } from 'react-hook-form';
+import { View, Text } from 'react-native';
+import { FormContainer } from 'src/components/Auth/FormContainer/FormContainer';
 import { CustomText } from 'src/components/Text/CustomText';
 
+import { RegisterFormStepOne } from '~/src/components/Auth/RegisterForm/RegisterFormStepOne/RegisterFormStepOne';
+import { GoBackButton } from '~/src/components/Button/GoBackButton/GoBackButton';
+
 export function RegisterStepTwoView() {
+
   return (
-    <Container className='items-center justify-center'>
-      <CustomText>Register Step Two</CustomText>
-    </Container>
+    <FormContainer className='items-center bg-tl-bg'>
+      <View className='mt-10 flex w-full flex-row items-center gap-36'>
+        <GoBackButton />
+        <View>
+          <Text className='text-white'>dynamic form test</Text>
+        </View>
+      </View>
+      <View>
+        
+        <RegisterFormStepOne />
+      </View>
+    </FormContainer>
   );
 }
