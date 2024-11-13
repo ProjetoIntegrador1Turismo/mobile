cb7b1af9dc6458998f709a42ed1fea285da043a9
Bernardo Tonin <btprates2004@gmail.com>
2024-11-12 21:34:00 -0300

    refactor: remove unused code

--- src/components/Auth/RegisterForm/RegisterFormStepOne/RegisterFormStepOneViewModel.ts
+++ src/components/Auth/RegisterForm/RegisterFormStepOne/RegisterFormStepOneViewModel.ts
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { Alert } from 'react-native';
 import { useAppRouter } from 'src/common/lib/router';
 import RegisterStepOneSchema from 'src/common/schemas/Register/RegisterStepOneSchema';
 import { RegisterStepOneFormData } from 'src/components/Auth/RegisterForm/RegisterFormStepOne/RegisterFormStepOne.types';
@@ -7,12 +8,7 @@ import { RegisterStepOneFormData } from 'src/components/Auth/RegisterForm/Regist
 export const useRegisterStepOneViewModel = () => {
   const { push } = useAppRouter();
 
-  const {
-    control,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<RegisterStepOneFormData>({
+  const { control, handleSubmit, watch } = useForm<RegisterStepOneFormData>({
     resolver: zodResolver(RegisterStepOneSchema),
     shouldUnregister: true,
     defaultValues: {
@@ -20,9 +16,11 @@ export const useRegisterStepOneViewModel = () => {
     },
   });
 
-  const onPressContinue = ({ name, phone }: RegisterStepOneFormData) => {
-    push(`/(auth)/register2?name=${name}&phone=${phone}`);
+  const isGuide = watch('isGuide');
+
+  const onPressContinue = (data: RegisterStepOneFormData) => {
+    push(`/(auth)/register2?name=${data.name}&phone=${data.phone}`);
   };
 
-  return { control, onPressContinue, handleSubmit, watch, errors };
+  return { control, onPressContinue, handleSubmit, isGuide };
 };
