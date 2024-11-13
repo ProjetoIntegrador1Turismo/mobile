0b46d2b5c463809194456b6227066787673dcb5f
Bernardo Tonin <btprates2004@gmail.com>
2024-11-12 21:32:22 -0300

    feat: add scrolling effect to form container

--- src/components/Auth/FormContainer/FormContainer.tsx
+++ src/components/Auth/FormContainer/FormContainer.tsx
@@ -1,4 +1,4 @@
-import { KeyboardAvoidingView, SafeAreaView, Platform } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, Platform, ScrollView } from 'react-native';
 import { cn } from 'src/common/utils/cn';
 import { FormContainerProps } from 'src/components/Auth/FormContainer/FormContainer.types';
 
@@ -7,7 +7,9 @@ export function FormContainer({ children, className }: FormContainerProps) {
     <KeyboardAvoidingView
       className='flex-1'
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <SafeAreaView className={cn('flex flex-1', className)}>{children}</SafeAreaView>
+      <ScrollView className='h-full flex-1 bg-tl-bg' overScrollMode='never'>
+        <SafeAreaView className={cn('flex flex-1', className)}>{children}</SafeAreaView>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 }
