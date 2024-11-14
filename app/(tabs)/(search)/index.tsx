fcd29fec8e50e21b43f8098e2f3e6f6f2a39714d
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-14 16:51:55 -0300

    chore: use Container and CategoryScreen on SearchTab

--- app/(tabs)/(search)/index.tsx
+++ app/(tabs)/(search)/index.tsx
@@ -3,9 +3,11 @@ import { useAppRouter } from 'src/common/lib/router';
 import { CustomText } from 'src/components/Text/CustomText';
 import { Avatar } from '~/src/components/Avatar/Avatar';
 import { CategoryCardList } from '~/src/components/CategoryCardList/CategoryCardList';
+import { Container } from '~/src/components/Container/Container';
 import { NotifiedInterestCard } from '~/src/components/NotifiedInterestCard/NotifiedInterestCard';
 import { SearchTextInputBar } from '~/src/components/SearchTextInputBar/SearchTextInputBar';
 import { UserAvatar } from '~/src/components/User/UserAvatar';
+import CategoryScreen from '~/src/screens/Category/CategoryScreen';
 
 export default function Search() {
   const { push } = useAppRouter();
@@ -15,17 +17,14 @@ export default function Search() {
   };
 
   return (
-    <SafeAreaView className='flex-1 items-center justify-center p-8'>
-      {/* <CustomText>Categories/Search Screen</CustomText>
-      <Pressable onPress={handlePress} className='rounded-lg border border-black p-3'>
-        <CustomText>Go to Point</CustomText>
-      </Pressable> */}            
-      <NotifiedInterestCard
-      userName='Carlos Alberto de Nobrega'
-      imageUrl='https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-      phone='(45) 99999-1111'
-      email='carlinhos@gmail.com'
-      itineraryTitle='Aventura nas Cataratas' />
-    </SafeAreaView>
+    // <SafeAreaView className='flex-1 items-center justify-center p-8'>
+    //   <CustomText>Categories/Search Screen</CustomText>
+    //   <Pressable onPress={handlePress} className='rounded-lg border border-black p-3'>
+    //     <CustomText>Go to Point</CustomText>
+    //   </Pressable>            
+    // </SafeAreaView>
+    <Container className='items-center justify-center p-8 bg-[#464646]'>
+      <CategoryScreen />
+    </Container>
   );
 }
