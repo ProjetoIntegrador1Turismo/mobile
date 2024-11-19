e7c199ee92401ccaf995958b7334a20a8424db83
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-19 11:22:25 -0300

    chore: create a Paginated component

---
+++ app/(tabs)/(dynamic,home,profile,search)/paginated.tsx
@@ -0,0 +1,29 @@
+import { useSegments, useLocalSearchParams } from 'expo-router';
+import { Pressable } from 'react-native';
+import { useAppRouter } from 'src/common/lib/router';
+import { Container } from 'src/components/Container/Container';
+import { CustomText } from 'src/components/Text/CustomText';
+
+export default function Paginated() {
+  const segments = useSegments();
+  const { goBack } = useAppRouter();
+  const currentTab = segments[1];
+  const { pointType } = useLocalSearchParams();
+
+  return (
+    <Container className='items-center justify-center'>
+      <CustomText size={36} weight='bold'>
+        Paginated Page
+      </CustomText>
+      <CustomText>
+        Viewing from <CustomText weight='bold'>{currentTab}</CustomText> tab
+      </CustomText>
+      <CustomText>
+        Parametro enviado <CustomText weight='bold'>{pointType}</CustomText> 
+      </CustomText>
+      <Pressable className='rounded-lg border border-black p-3' onPress={goBack}>
+        <CustomText>Voltar</CustomText>
+      </Pressable>
+    </Container>
+  );
+}
