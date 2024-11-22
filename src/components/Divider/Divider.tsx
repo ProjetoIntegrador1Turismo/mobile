97f3bcbb77cea0b4bf86894a39aac79219131817
Guilherme <guilherme_laz@hotmail.com>
2024-11-22 17:14:33 -0300

    chore: new weight config prop

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,12 +3,12 @@ import { View } from 'react-native';
 import { DividerProps } from 'src/components/Divider/Divider.types';
 import { CustomText } from '../Text/CustomText';
 
-export function Divider({ text }: DividerProps) {
+export function Divider({ text, weight }: DividerProps) {
   return (
     <View className="flex flex-row items-center my-4">
       <View className="flex-1 h-[1px] bg-white"/>
 
-      <CustomText className="text-white mx-2" size={14} weight='bold'>{text}</CustomText>
+      <CustomText className="text-white mx-2" size={14} weight={weight || 'regular'}>{text}</CustomText>
 
       <View className="flex-1 h-[1px] bg-white" />
     </View>
