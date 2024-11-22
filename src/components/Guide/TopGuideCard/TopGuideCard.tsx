efa790692eadf556f224f14cadc1970c44a92017
Guilherme <guilherme_laz@hotmail.com>
2024-11-22 17:15:29 -0300

    chore: removed bg color

--- src/components/Guide/TopGuideCard/TopGuideCard.tsx
+++ src/components/Guide/TopGuideCard/TopGuideCard.tsx
@@ -8,7 +8,7 @@ import { Stars } from 'src/components/Stars/Stars';
 
 export function TopGuideCard({ id, profileImage, name, rating }: TopGuideCardProps) {
   return (
-    <View className='flex h-[55px] w-[362px] flex-row items-center rounded-xl border border-white bg-[#1C1C1E] px-2 py-2'>
+    <View className='flex h-[55px] w-[362px] flex-row items-center rounded-xl border border-white px-2 py-2'>
       <Avatar imageUrl={profileImage} size={40} />
 
       <View className='ml-4  flex-auto overflow-hidden'>
