e1470dbf7d821d30b9772b099a731be22457d63d
Guilherme <guilherme_laz@hotmail.com>
2024-11-23 14:16:12 -0300

    chore: made component a pressable

--- src/components/InterestPoint/InterestPointCard/InterestPointCard.tsx
+++ src/components/InterestPoint/InterestPointCard/InterestPointCard.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Pressable, Text } from 'react-native';
 import { InterestPointCardProps } from 'src/components/InterestPoint/InterestPointCard/InterestPointCard.types';
 import { CustomText } from 'src/components/Text/CustomText';
 
-export function InterestPointCard({ id, name, imageCover }: InterestPointCardProps) {
+export function InterestPointCard({ id, name, imageCover, onPress }: InterestPointCardProps) {
   return (
-    <View className='h-[206px] w-[162px] overflow-hidden rounded-xl bg-black shadow-lg'>
-      <Image
-        source={{ uri: imageCover }}
-        style={{ width: '100%', height: '100%' }}
-        className='object-cover opacity-75'
-      />
-      <View className='absolute bottom-4 w-full px-2'>
-        <CustomText className='text-center text-2xl font-bold text-white' weight='regular'>
-          {name}
-        </CustomText>
+    <Pressable onPress={onPress}>
+      <View className='h-[206px] w-[162px] overflow-hidden rounded-xl bg-black shadow-lg'>
+        <Image
+          source={{ uri: imageCover }}
+          style={{ width: '100%', height: '100%' }}
+          className='object-cover opacity-75'
+        />
+        <View className='absolute bottom-4 w-full px-2'>
+          <Text numberOfLines={2} ellipsizeMode="tail">
+            <CustomText className='text-center text-2xl text-white' weight='regular'>
+              {name}
+            </CustomText>
+          </Text>
+        </View>
       </View>
-    </View>
+    </Pressable>
   );
 }
