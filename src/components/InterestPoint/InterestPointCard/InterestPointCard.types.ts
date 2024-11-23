e864236c0e35693c281d41e5802638be3efc3c26
Guilherme <guilherme_laz@hotmail.com>
2024-11-23 14:15:52 -0300

    chore: added onPress property to InterestPointCard props

--- src/components/InterestPoint/InterestPointCard/InterestPointCard.types.ts
+++ src/components/InterestPoint/InterestPointCard/InterestPointCard.types.ts
@@ -1,5 +1,8 @@
+import { GestureResponderEvent } from 'react-native';
+
 export interface InterestPointCardProps {
   id: number;
   name: string;
   imageCover: string;
+  onPress?: (event: GestureResponderEvent) => void;
 }
