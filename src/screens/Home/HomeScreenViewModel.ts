6ccd48e646f9c77799e5bdd95e8e228219441a94
Guilherme <guilherme_laz@hotmail.com>
2024-11-23 14:17:57 -0300

    feat: added HomeScreenViewModel for handling navigation

---
+++ src/screens/Home/HomeScreenViewModel.ts
@@ -0,0 +1,13 @@
+import { useAppRouter } from 'src/common/lib/router';
+
+export const useHomeScreenViewModel = () => {
+  const router = useAppRouter();
+
+  const handleInterestPointPress = (id: number) => {
+    router.push(`/(home)/point/${id}`);
+  };
+
+  return {
+    handleInterestPointPress,
+  };
+};
