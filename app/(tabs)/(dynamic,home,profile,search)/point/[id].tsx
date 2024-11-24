75bb659b1bdcd15f9962a0c547fcb3b9ebe5a039
Guilherme <guilherme_laz@hotmail.com>
2024-11-23 21:02:22 -0300

    feat: interest point screen

--- app/(tabs)/(dynamic,home,profile,search)/point/[id].tsx
+++ app/(tabs)/(dynamic,home,profile,search)/point/[id].tsx
@@ -1,8 +1,7 @@
 import { useLocalSearchParams, useSegments } from 'expo-router';
-import { Pressable } from 'react-native';
 import { useAppRouter } from 'src/common/lib/router';
-import { Container } from 'src/components/Container/Container';
-import { CustomText } from 'src/components/Text/CustomText';
+import { SafeAreaView } from 'react-native';
+import { InterestPointScreen } from '~/src/screens/InterestPoint/InterestPointScreen';
 
 export default function PointDetails() {
   const segments = useSegments();
@@ -11,19 +10,8 @@ export default function PointDetails() {
   const currentTab = segments[1];
 
   return (
-    <Container className='items-center justify-center'>
-      <CustomText size={36} weight='bold'>
-        Interest Point Details
-      </CustomText>
-      <CustomText>
-        Point ID: <CustomText weight='bold'>{id}</CustomText>
-      </CustomText>
-      <CustomText>
-        Viewing from <CustomText weight='bold'>{currentTab}</CustomText> tab
-      </CustomText>
-      <Pressable className='rounded-lg border border-black p-3' onPress={goBack}>
-        <CustomText>Voltar</CustomText>
-      </Pressable>
-    </Container>
+    <SafeAreaView className='flex-1 bg-tl-bg'>
+      <InterestPointScreen pointId={Number(id)} />
+    </SafeAreaView>
   );
 }
