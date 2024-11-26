7c1fba921ae0d6566ec2c3a78dc6b296e947a839
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-26 11:11:01 -0300

    cleanup: remove unnecessary imports

--- src/components/CategoryCardList/CategoryCard/CategoryCardViewModel.ts
+++ src/components/CategoryCardList/CategoryCard/CategoryCardViewModel.ts
@@ -1,13 +1,12 @@
 
 import { CategoryCardProps } from '~/src/components/CategoryCardList/CategoryCard/CategoryCard.types'
+import { useAppRouter } from '~/src/common/lib/router'
 
 import categoryHotelImg from '~/assets/category-card-hotel.png'
 import categoryExperienceImg from '~/assets/category-card-experience.png'
 import categoryEventImg from '~/assets/category-card-event.png'
 import categoryTouristPointlImg from '~/assets/category-card-tourist-point.png'
 import categoryRestaurantImg from '~/assets/category-card-restaurant.png'
-import { useAppRouter } from '~/src/common/lib/router'
-import { fetchPaginatedPoints } from '~/src/common/repositories/paginated/paginated.repository'
 
 export const CategoryCardData: CategoryCardProps[] = [
     { title: 'Hotel', imgSource: categoryHotelImg },
