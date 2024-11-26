9058afc4ad27b0ee22385399b4a769e85595ad84
Guilherme <guilherme_laz@hotmail.com>
2024-11-26 14:31:31 -0300

    chore: tweak customization

--- src/components/Price/Price.tsx
+++ src/components/Price/Price.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import { PriceProps } from 'src/components/Price/Price.types';
+import { PriceProps } from './Price.types';
 import { cn } from 'src/common/utils/cn';
 
-export function Price({ priceLevel, className }: PriceProps) {
+export function Price({ priceLevel, variant = 'light', size = 12, className }: PriceProps) {
+  const activeColor = variant === 'light' ? 'black' : 'white';
+  const inactiveColor = variant === 'light' ? '#B9B9B9' : 'rgba(255, 255, 255, 0.5)';
+
   return (
-    <View className={cn('flex flex-row space-x-1', className)}>
+    <View className={cn('flex flex-row', className)}>
       {Array.from({ length: 3 }, (_, index) => (
         <Feather
           key={index}
           name='dollar-sign'
-          size={12}
-          color={index < priceLevel ? 'black' : '#B9B9B9'}
+          size={size}
+          color={index < priceLevel ? activeColor : inactiveColor}
         />
       ))}
     </View>
