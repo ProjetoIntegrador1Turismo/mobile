1895292587aab4686b20cbac741a37f96a612a19
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-26 11:05:13 -0300

    chore: move CategoryDetailsView to a proper packge

--- src/screens/Category/CategoryDetails/CategoryDetailsView.tsx
+++ src/screens/Category/CategoryDetails/CategoryDetailsView.tsx
@@ -1,10 +1,12 @@
 import { Divider } from "~/src/components/Divider/Divider";
-import { CategoryDetailsProps } from "./CategoryDetailsView.types";
+import { CategoryDetailsProps } from '~/src/screens/Category/CategoryDetails/CategoryDetailsView.types';
 import { TouchableOpacity, View } from "react-native";
 import { CustomText } from "~/src/components/Text/CustomText";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { SearchTextInputBar } from "~/src/components/SearchTextInputBar/SearchTextInputBar";
 import { useAppRouter } from 'src/common/lib/router';
+import { BasicPointCard } from "~/src/components/Point/BasicPoint/BasicPointCard";
+
 
 
 export default function CategoryDetailsView({ categoryTitle }:CategoryDetailsProps){
@@ -14,11 +16,11 @@ export default function CategoryDetailsView({ categoryTitle }:CategoryDetailsPro
 
     return (
         <View>
-            <TouchableOpacity className='mt-6 mb-6' onPress={goBack}>
+            <TouchableOpacity className='mt-8 mb-4' onPress={goBack}>
                 <AntDesign name='arrowleft' size={30} color='white' />
             </TouchableOpacity>
             <SearchTextInputBar />
-            <View className='w-full items-center justify-center mt-6'>
+            <View className='w-full items-center justify-center mt-4'>
                 <CustomText
                 size={24}
                 weight='bold'
@@ -27,6 +29,7 @@ export default function CategoryDetailsView({ categoryTitle }:CategoryDetailsPro
                 </CustomText>
             <Divider text='Com base na categoria' ></Divider>
             </View>
+            <BasicPointCard id={5} pointName='Cataratas do Iguaçu do parque' />
         </View>    
     )
 }
