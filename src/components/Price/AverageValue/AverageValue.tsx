21f26099ae91d5093034e0a89d945206a121ade3
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-26 11:06:45 -0300

    feat: create AverageValue component to match dolar icon design

---
+++ src/components/Price/AverageValue/AverageValue.tsx
@@ -0,0 +1,23 @@
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { View } from 'react-native';
+import { AverageValueProps } from './AverageValue.types';
+import { useAverageValueIcons } from './AverageValueViewModel';
+
+
+export function AverageValue({ value }:AverageValueProps){
+
+    const icons = useAverageValueIcons(value);
+
+    return (
+        <>                    
+            {icons.map((_, index) => (
+                <FontAwesome6
+                    key={index}
+                    name="dollar-sign"
+                    size={16}
+                    color="white"
+                />
+            ))}        
+        </>
+    )
+}
