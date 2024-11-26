7ae9bc28fead315ef77ef2268662b0efc32ca1e6
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-26 17:14:22 -0300

    chore: insert BasicPointCard to work

--- src/screens/Category/CategoryDetails/CategoryDetailsScreen.tsx
+++ src/screens/Category/CategoryDetails/CategoryDetailsScreen.tsx
@@ -39,7 +39,14 @@ export default function CategoryDetailsScreen({ categoryTitle }:CategoryDetailsP
                     {categoryTitle}
                 </CustomText>
             <Divider text='Com base na categoria' ></Divider>
-            </View>            
+            </View>
+            <BasicPointCard
+                pointName="Cataratas do Iguacu"
+                id={2}
+                averageRating={2}
+                averageValue={200}
+                imageCover="https://plus.unsplash.com/premium_photo-1697729979889-31ec7ecf6f06?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />            
         </View>    
     )
 }
