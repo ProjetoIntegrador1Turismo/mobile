72f0b3a81b0e9cab8557791d6ae3987ceec4dcc8
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-28 16:57:34 -0300

    chore: disable load efect and use BasicPointCardList Mocks

--- src/screens/Category/CategoryDetails/CategoryDetailsScreen.tsx
+++ src/screens/Category/CategoryDetails/CategoryDetailsScreen.tsx
@@ -7,6 +7,7 @@ import { useAppRouter } from 'src/common/lib/router';
 import { useCategoryDetailsScreenViewModel } from "./CategoryDetailsScreenViewModel";
 import { BasicPointCard } from "~/src/components/Point/BasicPoint/BasicPointCard";
 import { GoBackButton } from "~/src/components/Button/GoBackButton/GoBackButton";
+import BasicPointCardList from "~/src/components/Point/BasicPointCardList";
 
 interface CategoryDetailsProps {
     categoryTitle: string;
@@ -41,13 +42,14 @@ export default function CategoryDetailsScreen({ categoryTitle }:CategoryDetailsP
                 </CustomText>
             <Divider text='Com base na categoria' ></Divider>
             </View>
-            <BasicPointCard
+            {/* <BasicPointCard
                 pointName="Cataratas do Iguacu"
                 id={2}
                 averageRating={2}
                 averageValue={200}
-                imageCover="https://plus.unsplash.com/premium_photo-1697729979889-31ec7ecf6f06?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />            
+                imageCover="https://picsum.photos/600/400"
+            />             */}
+            <BasicPointCardList />
         </View>    
     )
 }
--- src/screens/Category/CategoryDetails/CategoryDetailsScreenViewModel.ts
+++ src/screens/Category/CategoryDetails/CategoryDetailsScreenViewModel.ts
@@ -7,22 +7,22 @@ export function useCategoryDetailsScreenViewModel(categoryTitle: string){
     const [data, setData] = useState<PaginatedResponse | null>(null);
     const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            setLoading(true);
-            setError(null); // Reset de erro antes de cada chamada
-            try {
-                const result = await fetchPaginatedPoints(0, 5, categoryTitle); // Ajusta os parâmetros conforme necessário
-                setData(result);
-            } catch (err) {
-                setError("Erro ao buscar os dados. Tente novamente.");
-                console.error(err);
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchData();
-    }, [categoryTitle]);
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         setLoading(true);
+    //         setError(null); // Reset de erro antes de cada chamada
+    //         try {
+    //             const result = await fetchPaginatedPoints(0, 5, categoryTitle); // Ajusta os parâmetros conforme necessário
+    //             setData(result);
+    //         } catch (err) {
+    //             setError("Erro ao buscar os dados. Tente novamente.");
+    //             console.error(err);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     }
+    //     fetchData();
+    // }, [categoryTitle]);
 
     return {
         loading,
