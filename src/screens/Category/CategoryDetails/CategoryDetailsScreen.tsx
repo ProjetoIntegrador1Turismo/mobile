619f93a41f13136e4fec017932a963515a18b1fe
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-28 10:56:44 -0300

    chore: use builded GoBackButton to handle action goBack

--- src/screens/Category/CategoryDetails/CategoryDetailsScreen.tsx
+++ src/screens/Category/CategoryDetails/CategoryDetailsScreen.tsx
@@ -6,6 +6,7 @@ import { SearchTextInputBar } from "~/src/components/SearchTextInputBar/SearchTe
 import { useAppRouter } from 'src/common/lib/router';
 import { useCategoryDetailsScreenViewModel } from "./CategoryDetailsScreenViewModel";
 import { BasicPointCard } from "~/src/components/Point/BasicPoint/BasicPointCard";
+import { GoBackButton } from "~/src/components/Button/GoBackButton/GoBackButton";
 
 interface CategoryDetailsProps {
     categoryTitle: string;
@@ -17,18 +18,18 @@ export default function CategoryDetailsScreen({ categoryTitle }:CategoryDetailsP
     const { goBack } = useAppRouter();
 
 
-    if (loading) {
-        return (
-          <View className='flex-1 items-center justify-center'>
-            <ActivityIndicator size='large' />
-          </View>
-        );
-    }
+    // if (loading) {
+    //     return (
+    //       <View className='flex-1 items-center justify-center'>
+    //         <ActivityIndicator size='large' />
+    //       </View>
+    //     );
+    // }
 
     return (
         <View>
             <TouchableOpacity className='mt-8 mb-4' onPress={goBack}>
-                <AntDesign name='arrowleft' size={30} color='white' />
+                <GoBackButton />
             </TouchableOpacity>
             <SearchTextInputBar />
             <View className='w-full items-center justify-center mt-4'>
