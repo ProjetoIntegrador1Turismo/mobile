baf027aa08c5b3859fc5c4477a1d4e3e53346345
bernardotonin <btprates2004@gmail.com>
2024-11-29 20:02:45 -0300

    feat: add register step two form to register step two view

--- src/screens/Register/RegisterStepTwo/RegisterStepTwoView.tsx
+++ src/screens/Register/RegisterStepTwo/RegisterStepTwoView.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { useWatch } from 'react-hook-form';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { AuthFormHeader } from 'src/components/Auth/AuthFormHeader/AuthFormHeader';
 import { FormContainer } from 'src/components/Auth/FormContainer/FormContainer';
-import { CustomText } from 'src/components/Text/CustomText';
+import TLLogoWhite from 'src/components/Auth/TLLogoWhite/TLLogoWhite';
+import { GoBackButton } from 'src/components/Button/GoBackButton/GoBackButton';
+import { RegisterStepTwoViewProps } from 'src/screens/Register/RegisterStepTwo/RegisterStepTwo.types';
 
-import { RegisterFormStepOne } from '~/src/components/Auth/RegisterForm/RegisterFormStepOne/RegisterFormStepOne';
-import { GoBackButton } from '~/src/components/Button/GoBackButton/GoBackButton';
+import { RegisterFormStepTwo } from '~/src/components/Auth/RegisterForm/RegisterFormStepTwo/RegisterFormStepTwo';
 
-export function RegisterStepTwoView() {
+export function RegisterStepTwoView({ StepOneData }: RegisterStepTwoViewProps) {
   return (
     <FormContainer className='items-center bg-tl-bg'>
-      <View className='mt-10 flex w-full flex-row items-center gap-36'>
+      <View className='mt-14 flex w-11/12 gap-6'>
         <GoBackButton />
-        <View>
-          <Text className='text-white'>dynamic form test</Text>
-        </View>
+        <AuthFormHeader title='Criar Conta' label='Crie uma conta com suas informações!' />
       </View>
-      <View>
-        <RegisterFormStepOne />
+      <View className='mt-10'>
+        <RegisterFormStepTwo StepOneData={StepOneData} />
+      </View>
+      <View className='mt-[45px] '>
+        <TLLogoWhite className='h-[100px] w-[150px] object-cover' />
       </View>
     </FormContainer>
   );
