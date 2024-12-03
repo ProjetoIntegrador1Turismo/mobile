796c3fa48a413cc30c949495c6e373d76ed3fab7
bernardotonin <btprates2004@gmail.com>
2024-12-03 20:39:39 -0300

    refactor: change isLoading logic

--- src/components/Auth/LoginForm/LoginFormViewModel.ts
+++ src/components/Auth/LoginForm/LoginFormViewModel.ts
@@ -1,5 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useTransition } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useAppRouter } from 'src/common/lib/router';
 import LoginSchema from 'src/common/schemas/Login/LoginSchema';
@@ -7,7 +7,7 @@ import { useAuthStore } from 'src/common/stores/AuthStore';
 import { LoginFormData } from 'src/components/Auth/LoginForm/LoginForm.types';
 
 export const useLoginFormViewModel = () => {
-  const [isLoading, startTransition] = useTransition();
+  const [isLoading, setisLoading] = useState<boolean>();
   const { replace, push } = useAppRouter();
 
   const { login, isAuthenticated } = useAuthStore();
@@ -16,14 +16,10 @@ export const useLoginFormViewModel = () => {
     resolver: zodResolver(LoginSchema),
   });
 
-  const handleLoginAction = async (data: LoginFormData) => {
+  const onPressLogin = async (data: LoginFormData) => {
+    setisLoading(true);
     await login(data);
-  };
-
-  const onPressLogin = (data: LoginFormData) => {
-    startTransition(() => {
-      handleLoginAction(data);
-    });
+    setisLoading(false);
   };
 
   const onPressForgotPassword = () => {
