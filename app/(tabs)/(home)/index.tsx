9b0d8662887e5147acc76b7b84cfbf45bd07b54c
bernardotonin <btprates2004@gmail.com>
2024-12-03 20:35:46 -0300

    chore: remove tester code

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,38 +1,10 @@
-import { Pressable, SafeAreaView, Alert } from 'react-native';
-import { useAppRouter } from 'src/common/lib/router';
-import { useAuthStore } from 'src/common/stores/AuthStore';
+import { SafeAreaView } from 'react-native';
 import { CustomText } from 'src/components/Text/CustomText';
 
-import { getObject } from '~/src/common/lib/storage';
-import { Login } from '~/src/common/repositories/auth/auth.repository';
-
 export default function Home() {
-  const { push } = useAppRouter();
-  const user = useAuthStore((state) => state.user);
-  const { logout, error } = useAuthStore();
-
-  const handlePress = async () => {
-    await logout();
-  };
-
-  const handleStoragePress = async () => {
-    const data = await getObject('user');
-    Alert.alert(JSON.stringify(data));
-  };
-
   return (
-    <SafeAreaView className='flex-1 items-center justify-center p-8'>
-      <CustomText>Home</CustomText>
-      <Pressable onPress={handlePress} className='rounded-lg border border-black p-3'>
-        <CustomText>Logout</CustomText>
-      </Pressable>
-      <Pressable onPress={handleStoragePress} className='rounded-lg border border-black p-3'>
-        <CustomText>Get from storage</CustomText>
-      </Pressable>
-      <CustomText className='max-h-[250px] overflow-hidden'>
-        {JSON.stringify(user, null, 2)}
-      </CustomText>
-      <CustomText>{JSON.stringify(error, null, 2)}</CustomText>
+    <SafeAreaView className='flex-1 items-center justify-center bg-tl-bg p-8'>
+      <CustomText className='text-white'>Home</CustomText>
     </SafeAreaView>
   );
 }
--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -1,10 +1,8 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-
-import { useAppRouter } from '~/src/common/lib/router';
-import { useAuthStore } from '~/src/common/stores/AuthStore';
-import { SolidButton } from '~/src/components/Button/SolidButton/SolidButton';
-import { Container } from '~/src/components/Container/Container';
-import LoginView from '~/src/screens/Login/LoginView';
+import { useAppRouter } from 'src/common/lib/router';
+import { useAuthStore } from 'src/common/stores/AuthStore';
+import { SolidButton } from 'src/components/Button/SolidButton/SolidButton';
+import { Container } from 'src/components/Container/Container';
+import { CustomText } from 'src/components/Text/CustomText';
 
 export default function Profile() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
@@ -19,25 +17,13 @@ export default function Profile() {
           color='black'
           className='rounded-lg border border-black px-2'
         />
-        <SolidButton
-          onPress={() => push('/(auth)/register2')}
-          title='Go to register2'
-          color='black'
-          className='rounded-lg border border-black px-3'
-        />
       </Container>
     );
   }
 
   return (
-    <Container className='items-center justify-center'>
-      <Text>Voce esta autenticado!</Text>
+    <Container className='items-center justify-center bg-tl-bg'>
+      <CustomText className='text-white'>Voce esta autenticado!</CustomText>
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: '#FFF',
-  },
-});
