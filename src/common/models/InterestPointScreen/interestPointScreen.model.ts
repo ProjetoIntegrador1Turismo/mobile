729ba7eb18fa4ebb7d7fa898dc4d5182f58e53ec
Guilherme <guilherme_laz@hotmail.com>
2024-12-08 19:05:16 -0300

    feat: add interest point screen model

---
+++ src/common/models/InterestPointScreen/interestPointScreen.model.ts
@@ -0,0 +1,52 @@
+export interface InterestPointPageModel {
+    interestPoint: InterestPoint
+    guidesWhoOfferThisTour: GuidesWhoOfferThisTour[]
+    comments: Comment[]
+  }
+  
+  export interface InterestPoint {
+    id: number
+    name: string
+    address: Address
+    averageValue: number
+    shortDescription: string
+    longDescription: string
+    starsNumber: any
+    isResort: any
+    breakfastIncluded: any
+    foodType: any
+    date: any
+    duration: string
+    requiredAge: string
+    imageCoverUrl: string
+    images: string[]
+    averageRating: string
+  }
+  
+  export interface Address {
+    road: string
+    number: string
+    zipCode: string
+  }
+  
+  export interface GuidesWhoOfferThisTour {
+    id: number
+    firstName: string
+    lastName: string
+    averageRating: number
+    profileImageUrl: string
+  }
+  
+  export interface Comment {
+    id: number
+    text: string
+    wasVisitingDate: string
+    rating: number
+    tourist: Tourist
+  }
+  
+  export interface Tourist {
+    id: number
+    touristName: string
+    profileImageUrl: string
+  }
