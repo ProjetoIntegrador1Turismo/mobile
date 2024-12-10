9dd0b82e34062c2dea0aad147f94ad2998317329
bernardotonin <btprates2004@gmail.com>
2024-12-10 17:41:33 -0300

    refactor: move logic from profile route to profile view

--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -1,34 +1,5 @@
-import { useAppRouter } from 'src/common/lib/router';
-import { useAuthStore } from 'src/common/stores/AuthStore';
-import { SolidButton } from 'src/components/Button/SolidButton/SolidButton';
-import { Container } from 'src/components/Container/Container';
-import { CustomText } from 'src/components/Text/CustomText';
+import { ProfileView } from 'src/screens/Profile/ProfileView';
 
 export default function Profile() {
-  const { isAuthenticated, logout } = useAuthStore();
-  const { push } = useAppRouter();
-
-  if (!isAuthenticated) {
-    return (
-      <Container className='items-center justify-center gap-3'>
-        <SolidButton
-          onPress={() => push('/(auth)/login')}
-          title='Login'
-          className='rounded-lg px-2'
-        />
-      </Container>
-    );
-  }
-
-  return (
-    <Container className='items-center justify-center bg-tl-bg'>
-      <CustomText className='text-white'>Voce esta autenticado!</CustomText>
-      <SolidButton
-        onPress={() => logout()}
-        title='logout'
-        color='black'
-        className='rounded-lg border border-white px-2'
-      />
-    </Container>
-  );
+  return <ProfileView />;
 }
