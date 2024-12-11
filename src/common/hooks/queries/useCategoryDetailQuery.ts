9c95ade8c9df4f27e4d480fdb5e40e97cc362e24
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-11 15:18:08 -0300

    chore: create query to get paginated data  from api route

---
+++ src/common/hooks/queries/useCategoryDetailQuery.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query"
+import { CategoryDetailModel } from '~/src/common/models/Category/categoryDetail.model'
+import { fetchPaginatedPoints } from '~/src/common/repositories/paginated/paginated.repository'
+
+
+export const useCategoryDetailQuery = (pointType: string, page: number, size: number) => {
+    return useQuery<CategoryDetailModel>({
+        queryKey: ['category', pointType],
+        queryFn: () => fetchPaginatedPoints(pointType, page, size)
+    })
+}
