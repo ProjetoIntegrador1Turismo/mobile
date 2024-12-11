fd606770b1ea85913d9c1d206802a32407587071
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-11 16:18:02 -0300

    clean: remove unnecessary comments on category details viewModel

--- src/screens/Category/CategoryDetails/CategoryDetailsScreenViewModel.ts
+++ src/screens/Category/CategoryDetails/CategoryDetailsScreenViewModel.ts
@@ -13,28 +13,8 @@ export function useCategoryDetailsScreenViewModel(categoryTitle: string) {
     'Ponto Turístico': 'tourist-points'
 };
 
-console.log("Valor da string a ser enviada: ", pointRouteMapping[categoryTitle])
 const { data, isLoading } = useCategoryDetailQuery(pointRouteMapping[categoryTitle], 0, 5)
 
-console.log(data?.content[0])
-
-
-  // useEffect(() => {
-  //     async function fetchData() {
-  //         setLoading(true);
-  //         setError(null); // Reset de erro antes de cada chamada
-  //         try {
-  //             const result = await fetchPaginatedPoints(0, 5, categoryTitle); // Ajusta os parâmetros conforme necessário
-  //             setData(result);
-  //         } catch (err) {
-  //             setError("Erro ao buscar os dados. Tente novamente.");
-  //             console.error(err);
-  //         } finally {
-  //             setLoading(false);
-  //         }
-  //     }
-  //     fetchData();
-  // }, [categoryTitle]);
 
   return {
     isLoading,
