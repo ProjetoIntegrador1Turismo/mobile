d8c91ae3f75d8f028eb69b6589532cd165d0af96
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-16 12:09:41 -0300

    chore: update BasicPointCardList to use id as keyExtractor identifier

--- src/components/Point/BasicPointCardList.tsx
+++ src/components/Point/BasicPointCardList.tsx
@@ -7,7 +7,7 @@ export default function BasicPointCardList(data: CategoryDetailModel) {
   return (
     <FlatList
       data={data.content}
-      keyExtractor={(basicCard: Content) => basicCard.name}
+      keyExtractor={(basicCard: Content) => basicCard.id.toString()}
       renderItem={({ item }) => <BasicPointCard data={item} className='mb-[10px]' />}
       showsVerticalScrollIndicator={false}
       ListFooterComponent={<View className='h-[400px]' />}
