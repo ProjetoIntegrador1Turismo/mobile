1da92ff3ed811543996f7417670dfc9b3287664f
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-16 12:10:17 -0300

    chore: update api paginated route to get 15 itens

--- src/common/repositories/paginated/paginated.repository.ts
+++ src/common/repositories/paginated/paginated.repository.ts
@@ -3,7 +3,7 @@ import { CategoryDetailModel } from '~/src/common/models/Category/categoryDetail
 
 //Rota para paginados -> http://localhost:8081/paginated/events?page=4&size=2
 
-export async function fetchPaginatedPoints(pointType: string, page: number = 0, size: number = 5) { 
+export async function fetchPaginatedPoints(pointType: string, page: number = 0, size: number = 15) { 
         
     const { data } = await api.get<CategoryDetailModel>(
         `/paginated/${pointType}`,
