1da86eb586cc97a3c426d95a4468f681fd299114
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-17 15:52:40 -0300

    chore: update SearchTextInputBar to receive a function and link with onChangeText action

--- src/components/SearchTextInputBar/SearchTextInputBar.tsx
+++ src/components/SearchTextInputBar/SearchTextInputBar.tsx
@@ -2,25 +2,22 @@ import { Keyboard, TextInput, TouchableWithoutFeedback, View } from 'react-nativ
 import Feather from '@expo/vector-icons/Feather';
 import { useRef } from 'react';
 
-export function SearchTextInputBar() {
+interface SearchTextInputBarProps {
+  onChangeText?: (search: string) => void;
+}
+
+export function SearchTextInputBar({ onChangeText }: SearchTextInputBarProps) {
   const textInputRef = useRef<TextInput>(null);
 
-  const handlePressOutside = () => {
-    if (textInputRef.current?.isFocused()) {
-      Keyboard.dismiss();
-    } else {
-      textInputRef.current?.focus();
-    }
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={handlePressOutside}>
+    <TouchableWithoutFeedback>
       <View className='h-[40px] w-[362px] flex-row items-center rounded-[7px] bg-[#0A0A0A]'>
         <TextInput
           ref={textInputRef}
-          placeholder='Pesquisar'
+          placeholder='Filtrar'
           placeholderTextColor={'#464646'}
           className='flex-1 pl-[11px] text-white'
+          onChangeText={(text) => {if(onChangeText){onChangeText(text)}}}
         />
         <Feather name='search' size={24} color='white' className='pr-[12px]' />
       </View>
