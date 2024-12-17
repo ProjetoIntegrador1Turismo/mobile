892750f3af99df8e7d67add7da7aeeafd164eb10
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-17 15:57:55 -0300

    chore: add debounce useEffect and filter action to reload data when user types some search

--- src/screens/Category/CategoryDetails/CategoryDetailsScreenViewModel.ts
+++ src/screens/Category/CategoryDetails/CategoryDetailsScreenViewModel.ts
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useCategoryDetailQuery } from '~/src/common/hooks/queries/useCategoryDetailQuery';
-import { fetchPaginatedPoints } from '~/src/common/repositories/paginated/paginated.repository';
-import { PaginatedResponse } from '~/src/common/repositories/paginated/paginated.types';
+import { CategoryDetailModel, Content } from '~/src/common/models/Category/categoryDetail.model';
 
 export function useCategoryDetailsScreenViewModel(categoryTitle: string) {
   
@@ -14,11 +13,36 @@ export function useCategoryDetailsScreenViewModel(categoryTitle: string) {
     Roteiros: 'itineraries'
   };
 
-  const { data, isLoading } = useCategoryDetailQuery(pointRouteMapping[categoryTitle], 0)
+  const { data: originalData, isLoading  } = useCategoryDetailQuery(pointRouteMapping[categoryTitle], 0)
+  const [searchText, setSearchText] = useState('');
+  const [debouncedSearchText, setDebouncedSearchText] = useState('');
 
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedSearchText(searchText);
+    }, 1000); 
+
+    return () => clearTimeout(handler); // Limpa o timeout se o texto mudar antes de 1 segundo
+  }, [searchText]);
+
+  const filteredData: CategoryDetailModel | undefined = useMemo(() => {
+    if (!originalData || searchText.trim() === '') return originalData;
+
+    return {
+      ...originalData,
+      content: originalData.content.filter((item: Content) =>
+        (item.name || item.title || '').toLowerCase().includes(searchText.toLowerCase())
+      ),
+    };
+  }, [originalData, debouncedSearchText]);
+
+  const handleSearchAction = (search: string) => {
+    setSearchText(search);
+  }
 
   return {
     isLoading,
-    data,
+    data: filteredData,
+    handleSearchAction
   };
 }
