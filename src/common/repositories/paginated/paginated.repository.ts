995c23530c4e9a715437048318b82ffb17faa1f4
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-18 16:27:19 -0300

    chore: insert pointRouteMapping into paginated repository to handle API route mapping

--- src/common/repositories/paginated/paginated.repository.ts
+++ src/common/repositories/paginated/paginated.repository.ts
@@ -3,10 +3,24 @@ import { CategoryDetailModel } from '~/src/common/models/Category/categoryDetail
 
 //Rota para paginados -> http://localhost:8081/paginated/events?page=4&size=2
 
-export async function fetchPaginatedPoints(pointType: string, page: number = 0, size: number = 15) { 
+const pointRouteMapping: Record<string, string> = {
+    Hotel: 'hotels',
+    Experiência: 'experiences',
+    Restaurante: 'restaurants',
+    Evento: 'events',
+    'Ponto Turístico': 'tourist-points',
+    Roteiros: 'itineraries'
+  };
+
+function defineApiRoute(text:string): string{
+    console.log("String para map da rota: ", text)
+    return pointRouteMapping[text] || 'all'
+}
+
+export async function fetchPaginatedPoints(pointType: string, page: number = 0, size: number = 30) { 
         
     const { data } = await api.get<CategoryDetailModel>(
-        `/paginated/${pointType}`,
+        `/paginated/${defineApiRoute(pointType)}`,
         { params: { page, size } }
     );
     return data;  
