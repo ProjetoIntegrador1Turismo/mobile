1b21d32a714c46d3ba72d1004c40e7d9f61edb36
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-12-18 16:32:33 -0300

    feat: create function to deal with user search action on category screen

---
+++ src/screens/Category/CategoryViewViewModel.ts
@@ -0,0 +1,12 @@
+import { useAppRouter } from "~/src/common/lib/router";
+
+
+export function onMakeSearchOrder(){
+    const { push } = useAppRouter();
+    async function handleOnMakeSearchOrder(text:string){
+        push(`/(search)/paginated?pointType=${text}`);
+    }
+    return {
+        handleOnMakeSearchOrder
+    }
+}
