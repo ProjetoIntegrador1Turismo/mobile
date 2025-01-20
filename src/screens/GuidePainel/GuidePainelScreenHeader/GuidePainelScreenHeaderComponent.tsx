0ce6a4c62522392fb1a07169a2d25baeeb2d36bb
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2025-01-20 16:27:38 -0300

    chore: create header for guide dashbord screen to remove ScrollView and use only Flatlist.

---
+++ src/screens/GuidePainel/GuidePainelScreenHeader/GuidePainelScreenHeaderComponent.tsx
@@ -0,0 +1,36 @@
+import { View } from "react-native";
+import { GradientBorderButton } from "~/src/components/Button/GradientBorderButton/GradientBorderButton";
+import { LogoTl } from "~/src/components/Logo/LogoTL";
+import { CustomText } from "~/src/components/Text/CustomText";
+import { PeopleOutline } from "~/src/components/ExpoIcons/PeopleOutline";
+import { PlusIcon } from "~/src/components/ExpoIcons/Plus";
+
+export function GuidePainelScreenHeaderComponent(){
+    return (
+        <View className='bg-tl-bg p-4'>
+            <View className='items-center'>
+                <LogoTl />
+                <CustomText className='mt-4 text-white' weight='bold' size={36}>
+                    Painel do Guia!
+                </CustomText>
+                <CustomText className='mb-[21px] text-white' weight='light' size={14}>
+                    Gerencie aqui seus clientes
+                </CustomText>
+                <CustomText className='mb-[21px] text-white' weight='bold' size={20}>
+                    Opções:
+                </CustomText>
+            </View>
+            <View className="items-center mb-[29px]">
+                <View className="pb-[24px]">
+                    <GradientBorderButton title="Turistas" className="w-[190px]" icon={<PeopleOutline />} />
+                </View>
+                <View>
+                    <GradientBorderButton title="Criar Roteiro" className="w-[200px]" icon={<PlusIcon />} />            
+                </View>
+            </View>
+            <CustomText className="text-white" size={24} weight="bold">
+                Seus Roteiros:
+            </CustomText>            
+        </View>
+    )
+}
