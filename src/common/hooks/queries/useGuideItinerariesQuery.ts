3d07f9382437a0a537d1ea9100db967bad870574
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2025-01-20 16:11:13 -0300

    chore: create query to handle API guide itinerary data

---
+++ src/common/hooks/queries/useGuideItinerariesQuery.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
+import { GuideItinerariesModel } from '../../models/GuideItineraries/guideItineraries.model'
+import { fetchGuideItineraries } from '../../repositories/GuideItineraries/guideItineraries.repository'
+
+export const useGuideItinerariesQuery = (authToken: string) => {
+    return useQuery<GuideItinerariesModel>({
+        queryKey: ['guide-itineraries'],
+        queryFn: () => fetchGuideItineraries(authToken)
+    })
+}
