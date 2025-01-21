5d5241167c95972aae7e193620320ea20d10d23b
bernardotonin <btprates2004@gmail.com>
2025-01-21 19:16:59 -0300

    feat: add image to edit profile form

--- src/components/Auth/EditProfileForm/EditProfileForm.tsx
+++ src/components/Auth/EditProfileForm/EditProfileForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Pressable } from 'react-native';
 import { PhoneMask } from 'src/common/utils/masks';
 import { ControlledInput } from 'src/components/Auth/ControlledInput/ControlledInput';
 import { LoginFormData } from 'src/components/Auth/LoginForm/LoginForm.types';
@@ -10,15 +10,26 @@ import { CustomText } from 'src/components/Text/CustomText';
 
 import { EditProfileFormData } from './EditProfileForm.types';
 import { useEditProfileViewModel } from './EditProfileFormViewModel';
+import { Avatar } from '../../Avatar/Avatar';
 import { GoBackButton } from '../../Button/GoBackButton/GoBackButton';
 import { AuthFormHeader } from '../AuthFormHeader/AuthFormHeader';
 import { ControlledMaskedInput } from '../ControlledMaskedInput/ControlledMaskedInput';
 
 export function EditProfileForm() {
-  const { control, handleSubmit, onPressEdit } = useEditProfileViewModel();
+  const { control, handleSubmit, onPressEdit, pickImage, userProfilePicture } =
+    useEditProfileViewModel();
 
   return (
     <View className='flex items-center gap-1'>
+      <View className='flex items-center gap-2'>
+        <TouchableOpacity onPress={pickImage}>
+          <Image source={{ uri: userProfilePicture }} className='h-40 w-40 rounded-full' />
+        </TouchableOpacity>
+
+        <CustomText className='text-center text-white'>
+          Clique no avatar para adicionar uma foto!
+        </CustomText>
+      </View>
       <ControlledInput<EditProfileFormData>
         label='Nome'
         placeholder='Digite seu nome'
