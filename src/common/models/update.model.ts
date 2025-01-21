0abee8a0f9e91d343c27ab191d92f873282d3201
bernardotonin <btprates2004@gmail.com>
2025-01-21 19:15:17 -0300

    feat: create update user model

---
+++ src/common/models/update.model.ts
@@ -0,0 +1,12 @@
+export default interface UpdateModel {
+  id: number;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  cadasturCode: string;
+  profileImageUrl: string;
+  isApproved: string;
+  phone: string;
+  activeAdmin: boolean;
+}
