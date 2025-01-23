b42618c9dffabe6a93f33e982500a1641446bd8b
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2025-01-23 10:24:03 -0300

    chore: create screen to add necessary components to show tourists interested in the guide's itineraries

---
+++ src/screens/GuidePainel/InterestedTouristsItineraryScreen/InterestedTouristsItineraryScreen.tsx
@@ -0,0 +1,34 @@
+import { FlatList, View } from "react-native";
+import { InterestedTouristsHeader } from "./InterestedTouristsHeader/InterestedTouristsHeader";
+import { Itinerary } from "~/src/common/models/GuideItineraries/guideItineraries.model";
+import { useGuidePainel } from "../GuidePainelScreenViewModel";
+import { NotifiedInterestCard } from "~/src/components/NotifiedInterestCard/NotifiedInterestCard";
+
+interface InterestedTouristsItineraryScreenProps{
+    authToken: string;
+}
+
+
+export function InterestedTouristsItineraryScreen({ authToken }:InterestedTouristsItineraryScreenProps){
+
+    const { notifiedInterestCards } = useGuidePainel(authToken)
+
+    
+
+
+    return(
+        <FlatList
+        className="w-full"
+        data={notifiedInterestCards()}
+        ListHeaderComponent={<InterestedTouristsHeader />}
+        renderItem={({item}) => <NotifiedInterestCard
+         userName={item.userName}
+         imageUrl={item.imageUrl}
+         phone={item.phone}
+         email={item.email}
+         itineraryTitle={item.itineraryTitle}         
+         />}
+        
+        />
+    )
+}
