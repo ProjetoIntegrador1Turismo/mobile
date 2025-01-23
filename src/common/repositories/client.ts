8955beaf989160a76fc5cc0acc8b8a92cd77e429
bernardotonin <btprates2004@gmail.com>
2025-01-23 16:13:26 -0300

    feat: add intereceptor to append token to all requests

--- src/common/repositories/client.ts
+++ src/common/repositories/client.ts
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
+import { useAuthStore } from '../stores/AuthStore';
+
 export const BASE_URL = 'http://104.236.219.8:8081';
 
 export const api = axios.create({
   baseURL: BASE_URL,
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = await useAuthStore.getState().getAccessToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
