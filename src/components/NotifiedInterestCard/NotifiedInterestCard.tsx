915caea938ceea4c947c5d35b9c0048c20fa3250
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2025-01-23 11:51:37 -0300

    chore: update NotifiedInterestCard css config to centralize card

--- src/components/NotifiedInterestCard/NotifiedInterestCard.tsx
+++ src/components/NotifiedInterestCard/NotifiedInterestCard.tsx
@@ -15,37 +15,39 @@ export function NotifiedInterestCard({
   const { handleOnPressItineraryRef, openWhatsApp } = useNotifiedInterestCardViewModel();
 
   return (
-    <View className='h-[185px] w-[300px] rounded-[9px] border-2 bg-black'>
-      <View className='ml-[15px] mt-[11px]'>
-        <UserAvatar userName={userName} imageUrl={imageUrl} />
-      </View>
-      <View className='ml-[22px] mt-[11px]'>
-        <View className='flex flex-row'>
-          <CustomText className='w-[70px] text-white' weight='bold' size={12}>
-            Telefone:
-          </CustomText>
-          <TouchableOpacity onPress={() => openWhatsApp(phone)}>
-            <CustomText className='text-blue-500' size={12}>
-              {' '}
-              {phone}
+    <View className='items-center justify-center p-2'>
+      <View className='h-[185px] w-[300px] rounded-[9px] border-2 bg-black'>
+        <View className='ml-[15px] mt-[11px]'>
+          <UserAvatar userName={userName} imageUrl={imageUrl} />
+        </View>
+        <View className='ml-[22px] mt-[11px]'>
+          <View className='flex flex-row'>
+            <CustomText className='w-[70px] text-white' weight='bold' size={12}>
+              Telefone:
             </CustomText>
+            <TouchableOpacity onPress={() => openWhatsApp(phone)}>
+              <CustomText className='text-blue-500' size={12}>
+                {' '}
+                {phone}
+              </CustomText>
+            </TouchableOpacity>
+          </View>
+          <CustomText className='text-white' weight='bold' size={12}>
+            Email:
+            <CustomText size={12}> {email}</CustomText>
+          </CustomText>
+          <CustomText className='text-white' weight='bold' size={12}>
+            Roteiro:
+          </CustomText>
+          <TouchableOpacity
+            className='flex flex-row items-center'
+            onPress={() => handleOnPressItineraryRef(1)}>
+            <CustomText className='text-white' size={16}>
+              {itineraryTitle}
+            </CustomText>
+            <EvilIcons name='external-link' size={30} color='#3371E3' />
           </TouchableOpacity>
         </View>
-        <CustomText className='text-white' weight='bold' size={12}>
-          Email:
-          <CustomText size={12}> {email}</CustomText>
-        </CustomText>
-        <CustomText className='text-white' weight='bold' size={12}>
-          Roteiro:
-        </CustomText>
-        <TouchableOpacity
-          className='flex flex-row items-center'
-          onPress={() => handleOnPressItineraryRef(1)}>
-          <CustomText className='text-white' size={16}>
-            {itineraryTitle}
-          </CustomText>
-          <EvilIcons name='external-link' size={30} color='#3371E3' />
-        </TouchableOpacity>
       </View>
     </View>
   );
