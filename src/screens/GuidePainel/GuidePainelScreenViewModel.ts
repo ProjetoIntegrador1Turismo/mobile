22c2893df02dac79cf2f3e76d8f14bc3958835c3
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2025-01-23 10:14:25 -0300

    chore: create function to extract tourists interested in the guide's itineraries

--- src/screens/GuidePainel/GuidePainelScreenViewModel.ts
+++ src/screens/GuidePainel/GuidePainelScreenViewModel.ts
@@ -1,22 +1,37 @@
 import { useGuideItinerariesQuery } from "~/src/common/hooks/queries/useGuideItinerariesQuery";
-import { InterestedTourist, Itinerary } from "~/src/common/models/GuideItineraries/guideItineraries.model";
-
+import { GuideItinerariesModel, InterestedTourist, Itinerary } from "~/src/common/models/GuideItineraries/guideItineraries.model";
+import { useAppRouter } from '~/src/common/lib/router';
+import { NotifiedInterestCardProps } from "~/src/components/NotifiedInterestCard/NotifiedInterestCard.types";
 
 export function useGuidePainel(authToken:string){
 
     const { data, isLoading  } = useGuideItinerariesQuery(authToken);
-
     const itineraries: Itinerary[] = data || [];
+    const router = useAppRouter();
 
     const handleTouristButtonPress = () => {
-        //Abre o modal e carrega os turistas interessados do guia logado
+        //Abre o modal para turistas interessados do guia logado
         //Provisóriamente, passa o authToken 
-        console.log("testando.")
+        router.push(`(modals)/interested-itinerary-tourists?authToken=${authToken}`);
     }
+
+    const notifiedInterestCards = () => {
+        return itineraries.flatMap((itinerary) =>
+            itinerary.interestedTourists.map((tourist) => ({
+              userName: `${tourist.firstName} ${tourist.lastName}`, // Junção de firstName + lastName
+              imageUrl: tourist.profileImageUrl, // profileImageUrl do InterestedTourist
+              phone: tourist.phone, // phone do InterestedTourist
+              email: tourist.email, // email do InterestedTourist
+              itineraryTitle: itinerary.title, // title do Itinerary
+            }))
+          );
+    }
+
   
     return {
         itineraries,
         isLoading,
-        handleTouristButtonPress
+        handleTouristButtonPress,
+        notifiedInterestCards
     }
 }
