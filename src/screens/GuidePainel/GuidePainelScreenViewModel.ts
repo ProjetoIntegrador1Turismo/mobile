02c125e4df3b866fb057a5f968df78de880b4907
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2025-01-23 11:56:00 -0300

    chore: update imports and remove unnecessary comments

--- src/screens/GuidePainel/GuidePainelScreenViewModel.ts
+++ src/screens/GuidePainel/GuidePainelScreenViewModel.ts
@@ -1,7 +1,6 @@
-import { useGuideItinerariesQuery } from "~/src/common/hooks/queries/useGuideItinerariesQuery";
-import { GuideItinerariesModel, InterestedTourist, Itinerary } from "~/src/common/models/GuideItineraries/guideItineraries.model";
+import { useGuideItinerariesQuery } from '~/src/common/hooks/queries/useGuideItinerariesQuery';
+import { Itinerary } from '~/src/common/models/GuideItineraries/guideItineraries.model';
 import { useAppRouter } from '~/src/common/lib/router';
-import { NotifiedInterestCardProps } from "~/src/components/NotifiedInterestCard/NotifiedInterestCard.types";
 
 export function useGuidePainel(authToken:string){
 
@@ -15,18 +14,18 @@ export function useGuidePainel(authToken:string){
         router.push(`(modals)/interested-itinerary-tourists?authToken=${authToken}`);
     }
 
+    //Função que extrai os turistas interessados dos roteiros do guia logado
     const notifiedInterestCards = () => {
         return itineraries.flatMap((itinerary) =>
             itinerary.interestedTourists.map((tourist) => ({
-              userName: `${tourist.firstName} ${tourist.lastName}`, // Junção de firstName + lastName
-              imageUrl: tourist.profileImageUrl, // profileImageUrl do InterestedTourist
-              phone: tourist.phone, // phone do InterestedTourist
-              email: tourist.email, // email do InterestedTourist
-              itineraryTitle: itinerary.title, // title do Itinerary
+              userName: `${tourist.firstName} ${tourist.lastName}`, 
+              imageUrl: tourist.profileImageUrl, 
+              phone: tourist.phone, 
+              email: tourist.email, 
+              itineraryTitle: itinerary.title, 
             }))
-          );
+        );
     }
-
   
     return {
         itineraries,
