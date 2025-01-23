1d44544bae6459b368f41d30b2e18e49298369d9
bernardotonin <btprates2004@gmail.com>
2025-01-23 16:12:33 -0300

    feat: add getme query to profile view model

--- src/screens/Profile/ProfileViewModel.ts
+++ src/screens/Profile/ProfileViewModel.ts
@@ -1,9 +1,18 @@
 import { useAppRouter } from 'src/common/lib/router';
 import { useAuthStore } from 'src/common/stores/AuthStore';
 
+import { useGetMeQuery } from '~/src/common/hooks/queries/useGetMeQuery';
+
 export const useProfileViewModel = () => {
   const { isAuthenticated, logout, user } = useAuthStore();
   const { push } = useAppRouter();
+  const { data: GetMeData, isLoading } = useGetMeQuery(user?.email ?? '');
+
+  const userTypeMap: Record<string, string> = {
+    Tourist: 'Turista',
+    Guide: 'Guia de Turismo',
+    Admin: 'Administrador',
+  };
 
   const handleLoginPress = () => {
     push('/(auth)/login');
@@ -13,5 +22,18 @@ export const useProfileViewModel = () => {
     push('/(auth)/editprofile');
   };
 
-  return { isAuthenticated, handleLoginPress, logout, handlePressEdit, user };
+  const handleLogoutPress = () => {
+    logout();
+  };
+
+  return {
+    isAuthenticated,
+    handleLoginPress,
+    handleLogoutPress,
+    handlePressEdit,
+    user,
+    userTypeMap,
+    GetMeData,
+    isLoading,
+  };
 };
