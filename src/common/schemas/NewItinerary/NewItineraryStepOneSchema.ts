7d8e32d7fcf741d878163bdaabdc54b2af3476a4
bernardotonin <btprates2004@gmail.com>
2025-01-30 19:02:52 -0300

    refactor: improve validation messages in NewItineraryStepOneSchema

--- src/common/schemas/NewItinerary/NewItineraryStepOneSchema.ts
+++ src/common/schemas/NewItinerary/NewItineraryStepOneSchema.ts
@@ -2,9 +2,13 @@ import { z } from 'zod';
 
 const NewItineraryStepOneFormSchema = z.object({
   title: z.string({ required_error: 'Título é obrigatório! ' }).min(1, 'Título é obrigatório!'),
-  averageCost: z.string({ required_error: 'Custo médio é obrigatório! ' }).min(1, 'Título é obrigatório!'),
-  days: z.string({ required_error: 'Dias é obrigatório!' }),
-  description: z.string({ required_error: 'Descrição é obrigatória!' }),
+  averageCost: z
+    .string({ required_error: 'Custo médio é obrigatório! ' })
+    .min(1, 'Custo médio é obrigatório!'),
+  days: z.string({ required_error: 'Dias é obrigatório!' }).min(1, 'Dias é obrigatório!'),
+  description: z
+    .string({ required_error: 'Descrição é obrigatória!' })
+    .min(1, 'Descrição é obrigatória!'),
   imgCover: z.string(),
 });
 
