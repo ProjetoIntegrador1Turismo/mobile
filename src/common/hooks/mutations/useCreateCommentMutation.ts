f7217af3188b7e539b8b00569909bfbf2df41d2d
Guilherme <guilherme_laz@hotmail.com>
2025-01-30 14:28:20 -0300

    feat: create useCreateCommentMutation

---
+++ src/common/hooks/mutations/useCreateCommentMutation.ts
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { CreateComment } from 'src/common/repositories/comment/comment.repository';
+
+interface CreateCommentData {
+  text: string;
+  wasVisitingDate: string;
+  rating: number;
+}
+
+interface UseCreateCommentMutationParams {
+  pointId: number;
+  onSuccess?: () => void;
+  onError?: (error: any) => void;
+}
+
+export function useCreateCommentMutation({ pointId, onSuccess, onError }: UseCreateCommentMutationParams) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: CreateCommentData) => CreateComment(pointId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['tourPage', pointId]);
+      onSuccess?.();
+    },
+    onError: (error: any) => {
+      onError?.(error);
+    },
+  });
+}
