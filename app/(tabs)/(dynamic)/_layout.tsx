f22d10212c30f86cb1a924dc1c1067bbc1f55fac
bernardotonin <btprates2004@gmail.com>
2025-01-30 19:01:40 -0300

    feat: create itinerary form step one route

--- app/(tabs)/(dynamic)/_layout.tsx
+++ app/(tabs)/(dynamic)/_layout.tsx
@@ -5,6 +5,7 @@ export default function DynamicLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name='index' />
       <Stack.Screen name='point' />
+      <Stack.Screen name='create-itinerary-part-one' />
     </Stack>
   );
 }
---
+++ app/(tabs)/(dynamic)/create-itinerary-part-one.tsx
@@ -0,0 +1,5 @@
+import NewItineraryStepOneView from 'src/screens/NewItinerary/NewItineraryStepOneView';
+
+export default function NewItineraryStepOne() {
+  return <NewItineraryStepOneView />;
+}
