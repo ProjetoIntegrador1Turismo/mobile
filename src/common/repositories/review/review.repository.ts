de19bba4c3db1578f5f4c1894e56737c3a5f481d
Guilherme <guilherme_laz@hotmail.com>
2025-01-30 14:28:52 -0300

    feat: create review repository

---
+++ src/common/repositories/review/review.repository.ts
@@ -0,0 +1,16 @@
+import { api } from '../client';
+
+interface CreateReviewDTO {
+  text: string;
+  date: string;
+  rating: number;
+}
+
+export async function CreateReview(guideId: number, data: CreateReviewDTO) {
+  try {
+    const response = await api.post(`/review/${guideId}`, data);
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+}
