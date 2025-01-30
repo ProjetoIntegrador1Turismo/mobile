ed41aa1bfde5c2508767249b47281aaffc0b861f
Guilherme <guilherme_laz@hotmail.com>
2025-01-30 14:28:43 -0300

    feat: create comment repository

---
+++ src/common/repositories/comment/comment.repository.ts
@@ -0,0 +1,16 @@
+import { api } from '../client';
+
+interface CreateCommentDTO {
+  text: string;
+  wasVisitingDate: string;
+  rating: number;
+}
+
+export async function CreateComment(pointId: number, data: CreateCommentDTO) {
+  try {
+    const response = await api.post(`/comment/create/${pointId}`, data);
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+}
