1e506c96efda9a75cde989c73c1d98aa3ab9fb35
bernardotonin <btprates2004@gmail.com>
2025-02-02 14:46:01 -0300

    feat: add fetchInterestPointById function to retrieve interest point data by ID

--- src/common/repositories/InterestPoint/interestpoint.repository.ts
+++ src/common/repositories/InterestPoint/interestpoint.repository.ts
@@ -1,7 +1,13 @@
-import { api } from 'src/common/repositories/client';
 import { InterestPointPageModel } from 'src/common/models/InterestPointScreen/interestPointScreen.model';
+import { InterestPointByIdModel } from 'src/common/models/interestpointbyid.model';
+import { api } from 'src/common/repositories/client';
 
 export const fetchTourPageData = async (id: number) => {
   const { data } = await api.get<InterestPointPageModel>(`/page-source/tour/${id}`);
+  return data;
+};
+
+export const fetchInterestPointById = async (id: number) => {
+  const { data } = await api.get<InterestPointByIdModel>(`/interestpoint/${id}`);
   return data;
 };
