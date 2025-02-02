a1a96b167012569812a9a3c8d0783e04db4f41eb
bernardotonin <btprates2004@gmail.com>
2025-02-02 14:46:15 -0300

    feat: enhance NewItineraryStepOneFormSchema with title and description length validations

--- src/common/schemas/NewItinerary/NewItineraryStepOneSchema.ts
+++ src/common/schemas/NewItinerary/NewItineraryStepOneSchema.ts
@@ -1,15 +1,19 @@
 import { z } from 'zod';
 
 const NewItineraryStepOneFormSchema = z.object({
-  title: z.string({ required_error: 'Título é obrigatório! ' }).min(1, 'Título é obrigatório!'),
+  title: z
+    .string({ required_error: 'Título é obrigatório! ' })
+    .min(1, 'Título é obrigatório!')
+    .max(50, 'Título deve ter no máximo 50 caracteres!'),
   averageCost: z
     .string({ required_error: 'Custo médio é obrigatório! ' })
     .min(1, 'Custo médio é obrigatório!'),
   days: z.string({ required_error: 'Dias é obrigatório!' }).min(1, 'Dias é obrigatório!'),
   description: z
     .string({ required_error: 'Descrição é obrigatória!' })
-    .min(1, 'Descrição é obrigatória!'),
-  imgCover: z.string(),
+    .min(1, 'Descrição é obrigatória!')
+    .max(300, 'Descrição deve ter no máximo 300 caracteres!'),
+  imgCover: z.string({ required_error: 'Imagem de capa é obrigatória!' }),
 });
 
 export default NewItineraryStepOneFormSchema;
