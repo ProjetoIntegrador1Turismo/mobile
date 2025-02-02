6c5dff80d7e6c09bac45bdd135572a0eb9be2a0a
Guilherme <guilherme_laz@hotmail.com>
2025-02-02 20:31:10 -0300

    chore: added invalidate queries and toasts if error or success

--- src/common/hooks/mutations/useCreateItineraryMutation.ts
+++ src/common/hooks/mutations/useCreateItineraryMutation.ts
@@ -1,6 +1,26 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createNewItinerary } from 'src/common/repositories/Guide/guide.repository';
+import Toast from 'react-native-toast-message';
+
 
 export const useCreateItineraryMutation = () => {
-  return useMutation({ mutationFn: createNewItinerary });
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createNewItinerary,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['guide-itineraries'] });
+      queryClient.invalidateQueries({ queryKey: ['category', 'itinerary'] });
+      queryClient.invalidateQueries({ queryKey: ['guideProfile'] });
+      queryClient.invalidateQueries({ queryKey: ['homePage'] });
+    },
+    onError: () => {
+      Toast.show({
+        type: 'error',
+        text1: 'Erro ao criar roteiro',
+        text2: 'Tente novamente mais tarde',
+        position: 'top',
+      });
+    },
+  });
 };
