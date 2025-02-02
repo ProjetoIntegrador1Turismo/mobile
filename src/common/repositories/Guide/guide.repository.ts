ab7d338d3f2d9b38b08a47b2959fce0cf53e6589
bernardotonin <btprates2004@gmail.com>
2025-02-02 14:45:43 -0300

    feat: implement functions for creating itineraries and uploading cover images

--- src/common/repositories/Guide/guide.repository.ts
+++ src/common/repositories/Guide/guide.repository.ts
@@ -1,7 +1,66 @@
+import { Platform } from 'react-native';
+import { NewItineraryModel } from 'src/common/models/newitinerary.model';
 import { api } from 'src/common/repositories/client';
 import { GuideProfileModel } from 'src/screens/GuideProfile/GuideProfileScreen.types';
+import { v4 as uuidv4 } from 'uuid';
+
+import { CreateItineraryDTO } from './guide.types';
 
 export const fetchGuideProfileData = async (id: number) => {
   const { data } = await api.get<GuideProfileModel>(`/page-source/guide/${id}`);
   return data;
+};
+
+export const createNewItinerary = async (newItineraryData: CreateItineraryDTO) => {
+  const { averageCost, days, ...rest } = newItineraryData;
+
+  const { data } = await api.post<NewItineraryModel>(
+    '/itinerary',
+    JSON.stringify({
+      ...rest,
+      days: Number(days),
+      mediumCost: Number(averageCost),
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  return data;
+};
+
+export const uploadItineraryCoverImage = async ({
+  itineraryId,
+  uri,
+}: {
+  itineraryId: number;
+  uri: string;
+}) => {
+  if (!uri) {
+    throw new Error('URI inválido para o upload.');
+  }
+
+  const payload = new FormData();
+  const fileType = uri.split('.').pop();
+  payload.append('id', itineraryId.toString());
+  payload.append('file', {
+    uri: Platform.OS === 'android' ? uri : uri.replace('file://', ''),
+    type: `image/${fileType}`,
+    name: `${uuidv4()}.${fileType}`,
+  } as any);
+
+  try {
+    const { data } = await api.post<string>('/file/upload/itinerary', payload, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.error('Upload API error:', error);
+    throw error;
+  }
 };
