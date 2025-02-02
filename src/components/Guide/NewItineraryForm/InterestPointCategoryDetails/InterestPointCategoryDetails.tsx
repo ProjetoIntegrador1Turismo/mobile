5c3e7942cc2feae1d16954c1b38b99d5dbd3b83a
bernardotonin <btprates2004@gmail.com>
2025-02-02 14:47:47 -0300

    feat: add InterestPointCategoryDetails component for displaying category-specific interest points

---
+++ src/components/Guide/NewItineraryForm/InterestPointCategoryDetails/InterestPointCategoryDetails.tsx
@@ -0,0 +1,54 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { useAppRouter } from 'src/common/lib/router';
+
+import { InterestPointCard } from '../InterestPointCard/InterestPointCard';
+
+import { Content } from '~/src/common/models/Category/categoryDetail.model';
+import { GoBackButton } from '~/src/components/Button/GoBackButton/GoBackButton';
+import { Divider } from '~/src/components/Divider/Divider';
+import { SearchTextInputBar } from '~/src/components/SearchTextInputBar/SearchTextInputBar';
+import { CustomText } from '~/src/components/Text/CustomText';
+import { useCategoryDetailsScreenViewModel } from '~/src/screens/Category/CategoryDetails/CategoryDetailsScreenViewModel';
+
+interface CategoryDetailsProps {
+  categoryTitle: string;
+}
+
+export default function InterestPointCategoryDetails({ categoryTitle }: CategoryDetailsProps) {
+  const { data, isLoading, handleSearchAction } = useCategoryDetailsScreenViewModel(categoryTitle);
+
+  if (isLoading || !data) {
+    return (
+      <View className='flex h-screen items-center justify-center'>
+        <ActivityIndicator size='large' color='white' />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <GoBackButton className='mb-4 mt-8' />
+      <SearchTextInputBar onChangeText={handleSearchAction} />
+      <View className='mt-4 w-full items-center justify-center'>
+        <CustomText size={24} weight='bold' className='items-center justify-center text-white'>
+          {categoryTitle}
+        </CustomText>
+        <Divider text='Com base na categoria' />
+      </View>
+      <FlatList
+        data={data.content}
+        keyExtractor={(basicCard: Content) => basicCard.id.toString()}
+        renderItem={({ item }) => (
+          <InterestPointCard
+            onPressCard={(id: number) => console.log(id)}
+            data={item}
+            className='mb-[10px]'
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListFooterComponent={<View className='h-[500px]' />}
+      />
+    </View>
+  );
+}
