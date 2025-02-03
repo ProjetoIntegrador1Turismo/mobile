7bcdec8dfb8053e198d283efa3a3fee9b9041adf
Guilherme <guilherme_laz@hotmail.com>
2025-02-03 01:33:35 -0300

    chore: added error for not authenticated

--- src/common/repositories/InterestedItinerary/interested.repository.ts
+++ src/common/repositories/InterestedItinerary/interested.repository.ts
@@ -1,5 +1,7 @@
 import { InterestedItineraryModel } from '~/src/common/models/InterestedItinerary/InterestedItynerary.model';
 import { api } from '~/src/common/repositories/client';
+import { useAppRouter } from '~/src/common/lib/router';
+
 
 export async function fetchInterestedItinerary(authToken: string) {
   const { data } = await api.get<InterestedItineraryModel>('/user/me', {
@@ -20,12 +22,16 @@ export async function removeInterestedItinerary(itineraryId: number) {
 }
 
 export async function signalItineraryInterest(itineraryId: number) {
+
   try {
     await api.post(`/tourist/signal/${itineraryId}`);
     return true;
   } catch (error: any) {
     if (error.response?.status === 400) {
       throw new Error('Você já sinalizou interesse nesse roteiro!');
+    }
+    if (error.response?.status === 401) {
+      throw new Error('Você precisa estar logado para sinalizar interesse!');
     }
     throw error;
   }
