9f6dc61edb08e9f75ec2c9a2a189610acf071651
Guilherme <guilherme_laz@hotmail.com>
2025-02-05 00:37:54 -0300

    chore: fix navigation to itinerary and change icons

--- src/components/InterestedItinerary/InterestedItineraryCard/InterestedItineraryCard.tsx
+++ src/components/InterestedItinerary/InterestedItineraryCard/InterestedItineraryCard.tsx
@@ -2,8 +2,8 @@ import { Image, TouchableOpacity, View } from 'react-native';
 import { Avatar } from '../../Avatar/Avatar';
 import { CustomText } from '../../Text/CustomText';
 import { BASE_URL } from '~/src/common/repositories/client';
-import EvilIcons from '@expo/vector-icons/EvilIcons';
 import Feather from '@expo/vector-icons/Feather';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { useInterestedItineraryCardViewModel } from '~/src/components/InterestedItinerary/InterestedItineraryCard/InterestItineraryCardViewModel';
 import { InterestedItineraryCardProps } from '~/src/components/InterestedItinerary/InterestedItineraryCard/InterestedItineraryCard.types';
 
@@ -18,32 +18,36 @@ export function InterestedItineraryCard({
   const { truncatedName, handleSeeDetails } = useInterestedItineraryCardViewModel();
 
   return (
-    <View className='mt-[13px] flex h-[160px] w-[360px] items-center rounded-2xl rounded-md'>
-      <Image
-        source={{ uri: BASE_URL + itineraryImgUrl }}
-        className='h-full w-full rounded-2xl opacity-45'
-        resizeMode='cover'
-      />
-      <View className='absolute items-center'>
-        <View className='mt-4 h-auto w-auto flex-row items-center justify-center'>
-          <Avatar imageUrl={guideImgUrl} />
-          <View className='flex-row '>
-            <CustomText className='ml-2 text-white'>Guia: </CustomText>
-            <CustomText className='text-white' weight='bold'>
-              {truncatedName(guideName, 18)}
-            </CustomText>
+    <View className='px-4'>
+      <View className='flex h-[160px] items-center justify-center overflow-hidden rounded-xl bg-gradient-to-b from-[#1C1C1E] to-black shadow-lg'>
+        <Image
+          source={{ uri: BASE_URL + itineraryImgUrl }}
+          style={{ width: '100%', height: '100%' }}
+          className='object-cover opacity-25'
+        />
+        <View className='absolute inset-0 flex items-center justify-between p-4'>
+          <View className='mt-2 flex-row items-center'>
+            <Avatar imageUrl={guideImgUrl} />
+            <View className='flex-row'>
+              <CustomText className='ml-2 text-white'>Guia: </CustomText>
+              <CustomText className='text-white' weight='bold'>
+                {truncatedName(guideName, 18)}
+              </CustomText>
+            </View>
           </View>
-        </View>
-        <CustomText className='text-white' size={24} weight='bold'>
-          {truncatedName(itineraryTitle, 21)}
-        </CustomText>
-        <View className='flex-row'>
-          <TouchableOpacity>
-            <EvilIcons className='mr-12' name='external-link' size={45} color='#3371E3' />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onDelete}>
-            <Feather name='trash-2' size={32} color='#E22929' />
-          </TouchableOpacity>
+          <CustomText className='text-white' size={24} weight='bold'>
+            {truncatedName(itineraryTitle, 21)}
+          </CustomText>
+          <View className='flex-row items-center justify-center'>
+            <TouchableOpacity
+              onPress={() => handleSeeDetails(itineraryId)}
+              className='items-center justify-center px-6'>
+              <Feather name='external-link' size={32} color='#3371E3' />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onDelete} className='items-center justify-center px-6'>
+              <MaterialCommunityIcons name='trash-can' size={32} color='#FF3B30' />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
