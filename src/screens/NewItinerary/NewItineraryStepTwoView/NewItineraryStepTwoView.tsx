b188abc4347f029b77fb15033bf18426d8df949a
Guilherme <guilherme_laz@hotmail.com>
2025-02-06 12:18:16 -0300

    chore: added go back button

--- src/screens/NewItinerary/NewItineraryStepTwoView/NewItineraryStepTwoView.tsx
+++ src/screens/NewItinerary/NewItineraryStepTwoView/NewItineraryStepTwoView.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Container } from 'src/components/Container/Container';
 import TLLogoWhite from 'src/components/Auth/TLLogoWhite/TLLogoWhite';
 import { GradientBorderButton } from 'src/components/Button/GradientBorderButton/GradientBorderButton';
-import { Container } from 'src/components/Container/Container';
 import NewItineraryFormStepTwo from 'src/components/Guide/NewItineraryForm/NewItineraryFormStepTwo/NewItineraryFormStepTwo';
 import { CustomText } from 'src/components/Text/CustomText';
+import { GoBackButton } from 'src/components/Button/GoBackButton/GoBackButton';
 
 import { useNewItineraryStepTwoViewModel } from './NewItineraryStepTwoViewModel';
 
@@ -13,9 +14,12 @@ export default function NewItineraryStepTwoView() {
 
   return (
     <Container className='items-center gap-4 bg-tl-bg'>
+      <View className='absolute left-4 top-12 z-10'>
+        <GoBackButton />
+      </View>
       <View className='items-center gap-5 pt-12'>
         <TLLogoWhite className='h-16 w-20' />
-        <View>
+        <View className='items-center'>
           <CustomText className='text-center text-white' weight='bold' size={24}>
             Criação de Roteiros
           </CustomText>
@@ -24,14 +28,14 @@ export default function NewItineraryStepTwoView() {
           </CustomText>
         </View>
       </View>
-      <GradientBorderButton
-        title='Adicionar'
-        className='w-[90%]'
-        onPress={handleAddInsterestPointPress}
-      />
-      <View className='h-[400px]'>
-        <NewItineraryFormStepTwo />
-      </View>
+        <GradientBorderButton
+          title='Adicionar'
+          className='w-[90%]'
+          onPress={handleAddInsterestPointPress}
+        />
+        <View className='h-[400px]'>
+          <NewItineraryFormStepTwo />
+        </View>
     </Container>
   );
 }
