26ed615c3817bd303c31dbe7a1effe10c8418563
Guilherme <guilherme_laz@hotmail.com>
2025-02-06 12:18:51 -0300

    chore: invalidate queries after log out.
    FIX: When changing guides account, the second one was able to see and delete itineraries from previous user

--- src/screens/Profile/ProfileViewModel.ts
+++ src/screens/Profile/ProfileViewModel.ts
@@ -1,12 +1,13 @@
 import { useAppRouter } from 'src/common/lib/router';
 import { useAuthStore } from 'src/common/stores/AuthStore';
-
+import { useQueryClient } from '@tanstack/react-query';
 import { useGetMeQuery } from '~/src/common/hooks/queries/useGetMeQuery';
 
 export const useProfileViewModel = () => {
   const { isAuthenticated, logout, user } = useAuthStore();
   const { push } = useAppRouter();
   const { data: GetMeData, isLoading } = useGetMeQuery(user?.email ?? '');
+  const queryClient = useQueryClient();
 
   const userTypeMap: Record<string, string> = {
     Tourist: 'Turista',
@@ -23,7 +24,17 @@ export const useProfileViewModel = () => {
   };
 
   const handleLogoutPress = () => {
+    
+    queryClient.invalidateQueries({ queryKey: ['guide-itineraries'] });
+    queryClient.invalidateQueries({ queryKey: ['guideProfile'] });
+    queryClient.invalidateQueries({ queryKey: ['homePage'] });
+    queryClient.invalidateQueries({ queryKey: ['interested'] });
+    queryClient.invalidateQueries({ queryKey: ['tourPage'] });
+    queryClient.invalidateQueries({ queryKey: ['category'] });
+    queryClient.invalidateQueries({ queryKey: ['me'] });
+    queryClient.clear();
     logout();
+    
   };
 
   return {
