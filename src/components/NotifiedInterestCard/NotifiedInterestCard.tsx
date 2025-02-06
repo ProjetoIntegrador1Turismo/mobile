5e2c26f38d90e2ad5d84055abf438a3a8433e83a
Guilherme <guilherme_laz@hotmail.com>
2025-02-06 12:12:50 -0300

    chore: update screen implement link to itinerary

--- src/components/NotifiedInterestCard/NotifiedInterestCard.tsx
+++ src/components/NotifiedInterestCard/NotifiedInterestCard.tsx
@@ -1,8 +1,8 @@
-import { TouchableOpacity, View, Touchable } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { NotifiedInterestCardProps } from '~/src/components/NotifiedInterestCard/NotifiedInterestCard.types';
 import { UserAvatar } from '../User/UserAvatar';
 import { CustomText } from '../Text/CustomText';
-import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Feather from '@expo/vector-icons/Feather';
 import { useNotifiedInterestCardViewModel } from '~/src/components/NotifiedInterestCard/NotifiedInterestCardViewModel';
 
 export function NotifiedInterestCard({
@@ -11,41 +11,53 @@ export function NotifiedInterestCard({
   email,
   itineraryTitle,
   phone,
+  itineraryId,
 }: NotifiedInterestCardProps) {
   const { handleOnPressItineraryRef, openWhatsApp } = useNotifiedInterestCardViewModel();
 
   return (
-    <View className='items-center justify-center p-2'>
-      <View className='h-[185px] w-[300px] rounded-[9px] border-2 bg-black'>
-        <View className='ml-[15px] mt-[11px]'>
-          <UserAvatar userName={userName} imageUrl={imageUrl} />
-        </View>
-        <View className='ml-[22px] mt-[11px]'>
-          <View className='flex flex-row'>
-            <CustomText className='w-[70px] text-white' weight='bold' size={12}>
-              Telefone:
+    <View className='h-[160px] w-full rounded-[9px] border-2 border-gray-800 bg-[#1C1C1E] p-4'>
+      <View className='flex-row items-center space-x-3'>
+        <UserAvatar userName={userName} imageUrl={imageUrl} />
+        {/* <CustomText className='text-white' weight='bold' size={16}>
+          {userName}
+        </CustomText> */}
+      </View>
+
+      <View className='mt-4 space-y-2'>
+        <View className='flex-row items-center'>
+          <CustomText className='w-[70px] text-white' weight='bold' size={12}>
+            Telefone:
+          </CustomText>
+          <TouchableOpacity onPress={() => openWhatsApp(phone)}>
+            <CustomText className='text-blue-500' size={12}>
+              {phone}
             </CustomText>
-            <TouchableOpacity onPress={() => openWhatsApp(phone)}>
-              <CustomText className='text-blue-500' size={12}>
-                {' '}
-                {phone}
-              </CustomText>
-            </TouchableOpacity>
-          </View>
-          <CustomText className='text-white' weight='bold' size={12}>
+          </TouchableOpacity>
+        </View>
+
+        <View className='flex-row items-center'>
+          <CustomText className='w-[70px] text-white' weight='bold' size={12}>
             Email:
-            <CustomText size={12}> {email}</CustomText>
           </CustomText>
-          <CustomText className='text-white' weight='bold' size={12}>
-            Roteiro:
+          <CustomText className='text-white' size={12}>
+            {email}
           </CustomText>
-          <TouchableOpacity
-            className='flex flex-row items-center'
-            onPress={() => handleOnPressItineraryRef(1)}>
-            <CustomText className='text-white' size={16}>
+        </View>
+
+        <View className='flex-row items-center justify-between'>
+          <View className='flex-row items-center'>
+            <CustomText className='w-[70px] text-white' weight='bold' size={12}>
+              Roteiro:
+            </CustomText>
+            <CustomText className='text-white' size={12}>
               {itineraryTitle}
             </CustomText>
-            <EvilIcons name='external-link' size={30} color='#3371E3' />
+          </View>
+          <TouchableOpacity
+            className='items-center justify-center px-2'
+            onPress={() => handleOnPressItineraryRef(itineraryId)}>
+            <Feather name='external-link' size={24} color='#3371E3' />
           </TouchableOpacity>
         </View>
       </View>
